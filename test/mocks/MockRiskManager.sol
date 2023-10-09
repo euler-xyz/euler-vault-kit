5a1c300d73689a7d236540c61f12983ebae6e7cc
Doug Hoyte <doug@hcsw.org>
2023-10-09 09:07:26 -0400

    basic vault deposit test

--- test/mocks/MockRiskManager.sol
+++ test/mocks/MockRiskManager.sol
@@ -2,13 +2,38 @@
 
 pragma solidity ^0.8.0;
 
-contract MockRiskManager {
+interface IRiskManager {
+    struct MarketAssets {
+        address market;
+        uint assets;
+        bool assetsSet;
+    }
+
+    struct Snapshot {
+        uint112 totalBalances;
+        uint112 totalBorrows;
+    }
+}
+
+contract MockRiskManager is IRiskManager {
     constructor() {
     }
 
     function onMarketActivation(address, address, address, bytes calldata) external pure returns (bool success) {
         success = true;
     }
+
+    function checkLiquidity(address /*account*/, MarketAssets[] calldata /*collaterals*/, MarketAssets calldata /*liability*/) external pure returns (bool healthy) {
+		return true;
+	}
+
+	function checkMarketStatus(uint8 /*performedOperations*/, Snapshot memory /*oldSnapshot*/, Snapshot memory /*currentSnapshot*/) external pure returns (bool healthy, bytes memory notHealthyReason) {
+		return (true, "");
+	}
+
+	function computeInterestParams(address /*asset*/, uint32 /*utilisation*/) external pure returns (int96 interestRate, uint16 interestFee) {
+		return (0, 0);
+	}
 }
 
 contract MockRiskManagerFail {
