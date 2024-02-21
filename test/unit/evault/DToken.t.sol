e296b4e7c64cadb852424cac54e0a0f27c59e45c
totomanov <totomanov@protonmail.com>
2024-02-21 22:32:55 +0700

    test: pullDebt

--- test/unit/evault/DToken.t.sol
+++ test/unit/evault/DToken.t.sol
@@ -9,6 +9,7 @@ import {Events} from "src/EVault/shared/Events.sol";
 
 contract DTokenTest is EVaultTestBase {
     address user = makeAddr("user");
+    address user2 = makeAddr("user2");
 
     DToken dToken;
 
@@ -18,7 +19,11 @@ contract DTokenTest is EVaultTestBase {
         assetTST.mint(user, type(uint256).max);
         vm.prank(user);
         assetTST.approve(address(eTST), type(uint256).max);
-        
+
+        assetTST.mint(user2, type(uint256).max);
+        vm.prank(user2);
+        assetTST.approve(address(eTST), type(uint256).max);
+
         dToken = DToken(eTST.dToken());
     }
 
@@ -65,7 +70,7 @@ contract DTokenTest is EVaultTestBase {
 
         assertEq(dToken.balanceOf(user), 0);
         assertEq(dToken.totalSupply(), 0);
-        
+
         vm.expectEmit();
         emit Events.Transfer(address(0), user, amount);
         vm.prank(user);
@@ -95,7 +100,7 @@ contract DTokenTest is EVaultTestBase {
         eTST.deposit(amountBorrow, user);
         vm.prank(user);
         eTST.borrow(amountBorrow, user);
-        
+
         vm.expectEmit();
         emit Events.Transfer(user, address(0), amountRepay);
         vm.prank(user);
@@ -111,7 +116,7 @@ contract DTokenTest is EVaultTestBase {
         amountUnwind = bound(amountUnwind, 1, amountWind);
         vm.prank(user);
         eTST.wind(amountWind, user);
-        
+
         vm.expectEmit();
         emit Events.Transfer(user, address(0), amountUnwind);
         vm.prank(user);
@@ -121,8 +126,28 @@ contract DTokenTest is EVaultTestBase {
         assertEq(dToken.totalSupply(), amountWind - amountUnwind);
     }
 
-    function test_onPullDebt() public {
-        vm.skip(true);
+    function test_onPullDebt(uint256 amountBorrow, uint256 amountPull) public {
+        setUpCollateral();
+        amountBorrow = bound(amountBorrow, 1, MAX_SANE_AMOUNT / 10);
+        amountPull = bound(amountPull, 1, amountBorrow);
+        vm.prank(user);
+        eTST.deposit(amountBorrow, user);
+        vm.prank(user);
+        eTST.borrow(amountBorrow, user);
+
+        vm.prank(user2);
+        eTST.deposit(amountBorrow, user2);
+
+        vm.expectEmit();
+        emit Events.Transfer(user, address(0), amountPull);
+        vm.expectEmit();
+        emit Events.Transfer(address(0), user2, amountPull);
+        vm.prank(user2);
+        eTST.pullDebt(amountPull, user);
+
+        assertEq(dToken.balanceOf(user), amountBorrow - amountPull);
+        assertEq(dToken.balanceOf(user2), amountPull);
+        assertEq(dToken.totalSupply(), amountBorrow);
     }
 
     function test_onLiquidation() public {
@@ -136,12 +161,19 @@ contract DTokenTest is EVaultTestBase {
         assetTST2.mint(user, type(uint256).max);
         assetTST2.approve(address(eTST2), type(uint256).max);
         eTST2.deposit(MAX_SANE_AMOUNT / 100, user);
-
         evc.enableController(user, address(eTST));
         evc.enableCollateral(user, address(eTST2));
+        vm.stopPrank();
+
+        vm.startPrank(user2);
+        assetTST2.mint(user2, type(uint256).max);
+        assetTST2.approve(address(eTST2), type(uint256).max);
+        eTST2.deposit(MAX_SANE_AMOUNT / 100, user2);
+        evc.enableController(user2, address(eTST));
+        evc.enableCollateral(user2, address(eTST2));
+        vm.stopPrank();
 
         oracle.setPrice(address(assetTST), unitOfAccount, 1 ether);
         oracle.setPrice(address(eTST2), unitOfAccount, 1000 ether);
-        vm.stopPrank();
     }
 }
