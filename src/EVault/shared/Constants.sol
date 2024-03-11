d34fa19cfb3043494a350df6a00f6cf485237bc4
Doug Hoyte <doug@hcsw.org>
2024-03-11 02:47:40 -0400

    Interest rate overflows

--- src/EVault/shared/Constants.sol
+++ src/EVault/shared/Constants.sol
@@ -8,7 +8,7 @@ import {IVault as IEVCVault} from "ethereum-vault-connector/interfaces/IVault.so
 uint256 constant MAX_PROTOCOL_FEE_SHARE = 0.5 * 1e18;
 uint256 constant INITIAL_INTEREST_ACCUMULATOR = 1e27;
 uint256 constant SECONDS_PER_YEAR = 365.2425 * 86400; // Gregorian calendar
-uint256 constant MAX_ALLOWED_INTEREST_RATE = uint256(5 * 1e27) / SECONDS_PER_YEAR; // 500% APR
+uint256 constant MAX_ALLOWED_INTEREST_RATE = 291867236290754569706; // 1,000,000% APY
 
 // Implementation internals
 
