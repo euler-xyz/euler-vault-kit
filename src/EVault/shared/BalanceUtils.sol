34ab3c63e3353089c89523675388f56f46eed5b5
dglowinski <dariushg32111@gmail.com>
2024-03-13 17:30:05 +0100

    only emit event on zero transfer

--- src/EVault/shared/BalanceUtils.sol
+++ src/EVault/shared/BalanceUtils.sol
@@ -63,28 +63,30 @@ abstract contract BalanceUtils is Base {
     }
 
     function transferBalance(address from, address to, Shares amount) internal {
-        (Shares origFromBalance, bool fromBalanceForwarderEnabled) =
-            marketStorage.users[from].getBalanceAndBalanceForwarder();
-        (Shares origToBalance, bool toBalanceForwarderEnabled) = marketStorage.users[to].getBalanceAndBalanceForwarder();
-        if (origFromBalance < amount) revert E_InsufficientBalance();
-
-        Shares newFromBalance;
-        unchecked {
-            newFromBalance = origFromBalance - amount;
-        }
-        Shares newToBalance = origToBalance + amount;
-
-        // zero amount transfer should emit Transfer event and update the balance tracker
         if (!amount.isZero()) {
+            (Shares origFromBalance, bool fromBalanceForwarderEnabled) =
+                marketStorage.users[from].getBalanceAndBalanceForwarder();
+            (Shares origToBalance, bool toBalanceForwarderEnabled) = marketStorage.users[to].getBalanceAndBalanceForwarder();
+            if (origFromBalance < amount) revert E_InsufficientBalance();
+
+            Shares newFromBalance;
+            unchecked {
+                newFromBalance = origFromBalance - amount;
+            }
+            Shares newToBalance = origToBalance + amount;
+
+            // zero amount transfer should emit Transfer event and update the balance tracker
+
             marketStorage.users[from].setBalance(newFromBalance);
             marketStorage.users[to].setBalance(newToBalance);
-        }
 
-        if (fromBalanceForwarderEnabled) {
-            tryBalanceTrackerHook(from, newFromBalance.toUint(), isControlCollateralInProgress());
-        }
-        if (toBalanceForwarderEnabled) {
-            tryBalanceTrackerHook(to, newToBalance.toUint(), false);
+
+            if (fromBalanceForwarderEnabled) {
+                tryBalanceTrackerHook(from, newFromBalance.toUint(), isControlCollateralInProgress());
+            }
+            if (toBalanceForwarderEnabled) {
+                tryBalanceTrackerHook(to, newToBalance.toUint(), false);
+            }
         }
 
         emit Transfer(from, to, amount.toUint());
