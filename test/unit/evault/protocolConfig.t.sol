abd5c2a17e55d019bf33d5130e1ef6cfbcc757c0
Doug Hoyte <doug@hcsw.org>
2024-03-15 01:23:08 -0400

    more clean-up

--- test/unit/evault/protocolConfig.t.sol
+++ test/unit/evault/protocolConfig.t.sol
@@ -28,44 +28,44 @@ contract ERC4626Test_ProtocolConfig is EVaultTestBase {
         assertEq(eTST.interestFee(), DEFAULT_INTEREST_FEE);
 
         vm.expectRevert(Errors.E_BadFee.selector);
-        eTST.setInterestFee(0.005 * 10_000);
+        eTST.setInterestFee(0.005e4);
 
         vm.expectRevert(Errors.E_BadFee.selector);
-        eTST.setInterestFee(0.9 * 10_000);
+        eTST.setInterestFee(0.9e4);
 
-        eTST.setInterestFee(0.4 * 10_000);
-        assertEq(eTST.interestFee(), 0.4 * 10_000);
+        eTST.setInterestFee(0.4e4);
+        assertEq(eTST.interestFee(), 0.4e4);
     }
 
     function test_interestFees_extended() public {
         vm.prank(admin);
-        protocolConfig.setInterestFeeRange(address(eTST), true, 0.002 * 10_000, 0.6 * 10_000);
+        protocolConfig.setInterestFeeRange(address(eTST), true, 0.002e4, 0.6e4);
 
-        eTST.setInterestFee(0.005 * 10_000);
-        assertEq(eTST.interestFee(), 0.005 * 10_000);
-
-        vm.expectRevert(Errors.E_BadFee.selector);
-        eTST.setInterestFee(0.001 * 10_000);
-
-        eTST.setInterestFee(0.55 * 10_000);
-        assertEq(eTST.interestFee(), 0.55 * 10_000);
+        eTST.setInterestFee(0.005e4);
+        assertEq(eTST.interestFee(), 0.005e4);
 
         vm.expectRevert(Errors.E_BadFee.selector);
-        eTST.setInterestFee(0.65 * 10_000);
+        eTST.setInterestFee(0.001e4);
+
+        eTST.setInterestFee(0.55e4);
+        assertEq(eTST.interestFee(), 0.55e4);
+
+        vm.expectRevert(Errors.E_BadFee.selector);
+        eTST.setInterestFee(0.65e4);
     }
 
     function test_interestFees_maliciousProtocolConfig() public {
         vm.prank(admin);
-        protocolConfig.setInterestFeeRange(address(eTST), true, 0.8 * 10_000, 0.9 * 10_000);
+        protocolConfig.setInterestFeeRange(address(eTST), true, 0.8e4, 0.9e4);
 
         // Vault won't call into protocolConfig with reasonable interestFee
 
-        eTST.setInterestFee(0.35 * 10_000);
-        assertEq(eTST.interestFee(), 0.35 * 10_000);
+        eTST.setInterestFee(0.35e4);
+        assertEq(eTST.interestFee(), 0.35e4);
 
         // But will outside the always-valid range
 
         vm.expectRevert(Errors.E_BadFee.selector);
-        eTST.setInterestFee(0.55 * 10_000);
+        eTST.setInterestFee(0.55e4);
     }
 }
