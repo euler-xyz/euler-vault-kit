5c4d504512e2652b47f3f2bb91a808bf9160bc82
kasperpawlowski <25374117+kasperpawlowski@users.noreply.github.com>
2024-03-15 14:33:50 +0000

    add ERC20Collateral

--- src/ESynth/Esynth.sol
+++ src/ESynth/Esynth.sol
@@ -2,21 +2,17 @@
 
 pragma solidity ^0.8.0;
 
-import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
-import {ERC20Permit} from "openzeppelin/token/ERC20/extensions/ERC20Permit.sol";
 import {Ownable} from "openzeppelin/access/Ownable.sol";
-import {Context} from "openzeppelin/utils/Context.sol";
-import {ReentrancyGuard} from "openzeppelin/utils/ReentrancyGuard.sol";
 import {IESynth} from "./IESynth.sol";
-import {IEVC} from "ethereum-vault-connector/interfaces/IEthereumVaultConnector.sol";
-import {EVCUtil} from "ethereum-vault-connector/utils/EVCUtil.sol";
+import {ERC20Collateral, ERC20, Context} from "../ERC20Collateral/ERC20Collateral.sol";
+import {IEVC, EVCUtil} from "ethereum-vault-connector/utils/EVCUtil.sol";
 
 interface IVault {
     function increaseCash(uint256 amount) external;
     function decreaseCash(uint256 amount) external;
 }
 
-contract ESynth is EVCUtil, IESynth, ERC20Permit, Ownable, ReentrancyGuard {
+contract ESynth is IESynth, ERC20Collateral, Ownable {
     struct MinterData {
         uint128 capacity;
         uint128 minted;
@@ -27,9 +23,7 @@ contract ESynth is EVCUtil, IESynth, ERC20Permit, Ownable, ReentrancyGuard {
     event MinterCapacitySet(address indexed minter, uint256 capacity);
     error E_CapacityReached();
 
-    constructor(IEVC evc_, string memory name_, string memory symbol_) EVCUtil(evc_) ERC20(name_, symbol_) ERC20Permit(name_) Ownable(msg.sender) {
-
-    }
+    constructor(IEVC evc_, string memory name_, string memory symbol_) ERC20Collateral(evc_, name_, symbol_) Ownable(msg.sender) {}
 
     /// @notice Sets the minting capacity for a minter.
     /// @dev Can only be called by the owner of the contract.
@@ -92,47 +86,13 @@ contract ESynth is EVCUtil, IESynth, ERC20Permit, Ownable, ReentrancyGuard {
     function decreaseCash(address vault, uint256 amount) external onlyOwner nonReentrant {
         IVault(vault).decreaseCash(amount);
     }
-    
-    /// @notice Transfers a certain amount of tokens to a recipient.
-    /// @param to The recipient of the transfer.
-    /// @param amount The amount shares to transfer.
-    /// @return A boolean indicating whether the transfer was successful.
-    function transfer(address to, uint256 amount) public virtual override nonReentrant returns (bool) {
-        return super.transfer(to, amount);
-    }
 
-    /// @notice Transfers a certain amount of tokens from a sender to a recipient.
-    /// @param from The sender of the transfer.
-    /// @param to The recipient of the transfer.
-    /// @param amount The amount of shares to transfer.
-    /// @return A boolean indicating whether the transfer was successful.
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) public virtual override nonReentrant returns (bool) {
-        return super.transferFrom(from, to, amount);
-    }
-
-    /// @notice Transfers a `value` amount of tokens from `from` to `to`, or alternatively mints (or burns) if `from`
-    /// (or `to`) is the zero address. All customizations to transfers, mints, and burns should be done by overriding
-    /// this function.
-    /// @dev Overriden to require account status checks on transfers from non-zero addresses. The account status check
-    /// must be required on any operation that reduces user's balance. Note that the user balance cannot be modified
-    /// after the account status check is required. If that's the case, the contract must be modified so that the
-    /// account status check is required as the very last operation of the function.
-    /// @param from The address from which tokens are transferred or burned.
-    /// @param to The address to which tokens are transferred or minted.
-    /// @param value The amount of tokens to transfer, mint, or burn.
-    function _update(address from, address to, uint256 value) internal virtual override {
-        super._update(from, to, value);
-
-        if (from != address(0)) {
-            evc.requireAccountStatusCheck(from);
-        }
-    }
-
-    function _msgSender() internal view override(Context, EVCUtil) returns (address sender) {
-        return EVCUtil._msgSender();
+    /// @notice Retrieves the message sender in the context of the EVC.
+    /// @dev Overriden due to the conflict with the Context definition.
+    /// @dev This function returns the account on behalf of which the current operation is being performed, which is
+    /// either msg.sender or the account authenticated by the EVC.
+    /// @return The address of the message sender.
+    function _msgSender() internal view virtual override(ERC20Collateral, Context) returns (address) {
+        return ERC20Collateral._msgSender();
     }
 }
