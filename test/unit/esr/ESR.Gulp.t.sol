ac0edd9060f2aa01d6261e8f8985c1a0b7ad0ac2
Rangel Stoilov <rangelstoilov@gmail.com>
2024-03-15 14:26:56 +0200

    feat: move ESR to vaults

---
+++ test/unit/esr/ESR.Gulp.t.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "./lib/ESRTest.sol";
+
+contract ESRGulpTest is ESRTest {
+    // TODO constructor tests
+    function testGulpNoPreviousInterest() public {
+        uint256 depositAmount = 100e18;
+        doDeposit(user, depositAmount);
+
+        uint256 interestAmount = 10e18;
+        // Mint interest directly into the contract
+        asset.mint(address(esr), interestAmount);
+        esr.gulp();
+
+        ESR.ESRSlot memory esrSlot = esr.getESRSlot();
+
+        assertEq(esr.totalAssets(), depositAmount);
+        assertEq(esrSlot.interestLeft, interestAmount);
+        assertEq(esrSlot.lastInterestUpdate, block.timestamp);
+        assertEq(esrSlot.interestSmearEnd, block.timestamp + esr.INTEREST_SMEAR());
+    }
+
+    function testDoubleGulpNoTimePassed() public {
+        uint256 depositAmount = 100e18;
+        doDeposit(user, depositAmount);
+
+        uint256 interestAmount = 10e18;
+        // Mint interest directly into the contract
+        asset.mint(address(esr), interestAmount);
+        esr.gulp();
+        // Mint interest directly into the contract
+        asset.mint(address(esr), interestAmount);
+        esr.gulp();
+
+        ESR.ESRSlot memory esrSlot = esr.getESRSlot();
+
+        assertEq(esr.totalAssets(), depositAmount);
+        assertEq(esrSlot.interestLeft, interestAmount * 2);
+        assertEq(esrSlot.lastInterestUpdate, block.timestamp);
+        assertEq(esrSlot.interestSmearEnd, block.timestamp + esr.INTEREST_SMEAR());
+    }
+
+    function testDoubleGulpHalfOfPreviousDistributed() public {
+        uint256 depositAmount = 100e18;
+        doDeposit(user, depositAmount);
+
+        uint256 interestAmount = 10e18;
+        // Mint interest directly into the contract
+        asset.mint(address(esr), interestAmount);
+        esr.gulp();
+        skip(esr.INTEREST_SMEAR() / 2);
+        // Mint interest directly into the contract
+        asset.mint(address(esr), interestAmount);
+        esr.gulp();
+
+        ESR.ESRSlot memory esrSlot = esr.getESRSlot();
+
+        assertEq(esr.totalAssets(), depositAmount + interestAmount / 2);
+        assertEq(esrSlot.interestLeft, interestAmount + interestAmount / 2);
+        assertEq(esrSlot.lastInterestUpdate, block.timestamp);
+        assertEq(esrSlot.interestSmearEnd, block.timestamp + esr.INTEREST_SMEAR());
+    }
+
+    function testDoubleGulpPreviousDistributed() public {
+        uint256 depositAmount = 100e18;
+        doDeposit(user, depositAmount);
+
+        uint256 interestAmount = 10e18;
+        // Mint interest directly into the contract
+        asset.mint(address(esr), interestAmount);
+        esr.gulp();
+        skip(esr.INTEREST_SMEAR());
+        // Mint interest directly into the contract
+        asset.mint(address(esr), interestAmount);
+        esr.gulp();
+
+        ESR.ESRSlot memory esrSlot = esr.getESRSlot();
+
+        assertEq(esr.totalAssets(), depositAmount + interestAmount);
+        assertEq(esrSlot.interestLeft, interestAmount);
+        assertEq(esrSlot.lastInterestUpdate, block.timestamp);
+        assertEq(esrSlot.interestSmearEnd, block.timestamp + esr.INTEREST_SMEAR());
+    }
+}
