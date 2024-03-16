2521d6cd527295133c26e3a3ed7131be59dc237b
Mick de Graaf <mick@mickdegraaf.nl>
2024-03-16 14:42:04 +0100

    Refactor PSM to use ERC20 as underlying

--- src/pegStabilityModules/PSM.sol
+++ src/pegStabilityModules/PSM.sol
@@ -4,61 +4,63 @@ pragma solidity ^0.8.0;
 
 import {IESynth} from "../ESynth/IESynth.sol";
 import {EVCUtil, IEVC} from "ethereum-vault-connector/utils/EVCUtil.sol";
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
 contract PSM is EVCUtil {
+    using SafeERC20 for IERC20;
+
     uint public constant BPS_SCALE = 10000;
 
     IESynth public immutable synth;
+    IERC20 public immutable underlying;
+
     uint public immutable TO_UNDERLYING_FEE;
     uint public immutable TO_SYNTH_FEE;
 
     error E_TranferFailed();
 
-    constructor(address _evc, address _synth, uint toUnderlyingFeeBPS, uint toSynthFeeBPS) EVCUtil(IEVC(_evc)) {
+    constructor(address _evc, address _synth, address _underlying, uint toUnderlyingFeeBPS, uint toSynthFeeBPS) EVCUtil(IEVC(_evc)) {
         synth = IESynth(_synth);
+        underlying = IERC20(_underlying);
         TO_UNDERLYING_FEE = toUnderlyingFeeBPS;
         TO_SYNTH_FEE = toSynthFeeBPS;
     }
 
 
-    function swapToUnderlyingGivenIn(uint amountIn, address receiver) external payable returns (uint amountOut) {
+    function swapToUnderlyingGivenIn(uint amountIn, address receiver) external returns (uint amountOut) {
         amountOut = quoteToUnderlyingGivenIn(amountIn);
-        synth.burn(_msgSender(), amountIn);
 
-        (bool success, ) = receiver.call{value: amountOut}("");
-        if(!success) revert E_TranferFailed();
+        synth.burn(_msgSender(), amountIn);
+        underlying.safeTransfer(receiver, amountOut);
 
         return amountOut;
     }
 
-    function swapToUnderlyingGivenOut(uint amountOut, address receiver) external payable returns (uint amountIn) {
+    function swapToUnderlyingGivenOut(uint amountOut, address receiver) external returns (uint amountIn) {
         amountIn = quoteToUnderlyingGivenOut(amountOut);
-        synth.burn(_msgSender(), amountIn);
 
-        (bool success, ) = receiver.call{value: amountOut}("");
-        if(!success) revert E_TranferFailed();
+        synth.burn(_msgSender(), amountIn);
+        underlying.safeTransfer(receiver, amountOut);
 
         return amountIn;
     }
 
-    function swapToSynthGivenIn(address receiver) external payable returns (uint amountOut) {
-        amountOut = quoteToSynthGivenIn(msg.value);
+    function swapToSynthGivenIn(uint256 amountIn, address receiver) external returns (uint amountOut) {
+        amountOut = quoteToSynthGivenIn(amountIn);
+
+        underlying.safeTransferFrom(_msgSender(), address(this), amountIn);
         synth.mint(receiver, amountOut);
+
         return amountOut;
     }
 
-    function swapToSynthGivenOut(uint amountOut, address receiver) external payable returns (uint amountIn) {
+    function swapToSynthGivenOut(uint amountOut, address receiver) external returns (uint amountIn) {
         amountIn = quoteToSynthGivenOut(amountOut);
         
-        // Will throw on overflow if msg.value is less than expected amount in
-        uint delta = msg.value - amountIn;
-        // return excess
-        if (delta > 0) {
-            (bool success, ) = _msgSender().call{value: delta}("");
-            if(!success) revert E_TranferFailed();
-        }
-
+        underlying.safeTransferFrom(_msgSender(), address(this), amountIn);
         synth.mint(receiver, amountOut);
+
         return amountIn;
     }
 
--- test/unit/pegStabilityModules/PSM.t.sol
+++ test/unit/pegStabilityModules/PSM.t.sol
@@ -7,7 +7,9 @@ import "forge-std/console.sol";
 
 import {PSM} from "../../../src/pegStabilityModules/PSM.sol";
 import {ESynth, IEVC} from "../../../src/ESynth/ESynth.sol";
+import {TestERC20} from "../../mocks/TestERC20.sol";
 import {EthereumVaultConnector} from "ethereum-vault-connector/EthereumVaultConnector.sol";
+
 
 contract PSMTest is Test {
     // TODO tests where ETH transfers fail
@@ -17,6 +19,8 @@ contract PSMTest is Test {
     uint public BPS_SCALE = 10000;
 
     ESynth public synth;
+    TestERC20 public underlying;
+
     PSM public psm;
 
     IEVC public evc;
@@ -29,18 +33,27 @@ contract PSMTest is Test {
         // Deploy EVC
         evc = new EthereumVaultConnector();
 
+        // Deploy underlying
+        underlying = new TestERC20("TestUnderlying", "TUNDERLYING", 18, false);
+
         // Deploy synth
         vm.prank(owner);
         synth = new ESynth(evc, "TestSynth", "TSYNTH");
 
         // Deploy PSM
         vm.prank(owner);
-        psm = new PSM(address(evc), address(synth), TO_UNDERLYING_FEE, TO_SYNTH_FEE);
+        psm = new PSM(address(evc), address(synth), address(underlying), TO_UNDERLYING_FEE, TO_SYNTH_FEE);
 
-        // Give PSM and wallets some ETH
-        vm.deal(address(psm), 100e18);
-        vm.deal(wallet1, 100e18);
-        vm.deal(wallet2, 100e18);
+        // Give PSM and wallets some underlying
+        underlying.mint(address(psm), 100e18);
+        underlying.mint(wallet1, 100e18);
+        underlying.mint(wallet2, 100e18);
+
+        // Approve PSM to spend underlying
+        vm.prank(wallet1);
+        underlying.approve(address(psm), 100e18);
+        vm.prank(wallet2);
+        underlying.approve(address(psm), 100e18);
 
         // Set PSM as minter
         vm.prank(owner);
@@ -72,19 +85,19 @@ contract PSMTest is Test {
         uint expectedAmountOut = amountIn * (BPS_SCALE - TO_UNDERLYING_FEE) / BPS_SCALE;
 
         uint swapperSynthBalanceBefore = synth.balanceOf(wallet1);
-        uint receiverBalanceBefore = wallet2.balance;
-        uint psmETHBalanceBefore = address(psm).balance;
+        uint receiverBalanceBefore = underlying.balanceOf(wallet2);
+        uint psmUnderlyingBalanceBefore = underlying.balanceOf(address(psm));
 
         vm.prank(wallet1);
         psm.swapToUnderlyingGivenIn(amountIn, wallet2);
 
         uint swapperSynthBalanceAfter = synth.balanceOf(wallet1);
-        uint receiverBalanceAfter = wallet2.balance;
-        uint psmETHBalanceAfter = address(psm).balance;
+        uint receiverBalanceAfter = underlying.balanceOf(wallet2);
+        uint psmUnderlyingBalanceAfter = underlying.balanceOf(address(psm));
 
         assertEq(swapperSynthBalanceAfter, swapperSynthBalanceBefore - amountIn);
         assertEq(receiverBalanceAfter, receiverBalanceBefore + expectedAmountOut);
-        assertEq(psmETHBalanceAfter, psmETHBalanceBefore - expectedAmountOut);
+        assertEq(psmUnderlyingBalanceAfter, psmUnderlyingBalanceBefore - expectedAmountOut);
     }
 
     function testSwapToUnderlyingGivenOut() public {
@@ -92,69 +105,59 @@ contract PSMTest is Test {
         uint expectedAmountIn = amountOut * BPS_SCALE / (BPS_SCALE - TO_UNDERLYING_FEE);
 
         uint swapperSynthBalanceBefore = synth.balanceOf(wallet1);
-        uint receiverBalanceBefore = wallet2.balance;
-        uint psmETHBalanceBefore = address(psm).balance;
+        uint receiverBalanceBefore = underlying.balanceOf(wallet2);
+        uint psmUnderlyingBalanceBefore = underlying.balanceOf(address(psm));
 
         vm.prank(wallet1);
         psm.swapToUnderlyingGivenOut(amountOut, wallet2);
 
         uint swapperSynthBalanceAfter = synth.balanceOf(wallet1);
-        uint receiverBalanceAfter = wallet2.balance;
-        uint psmETHBalanceAfter = address(psm).balance;
+        uint receiverBalanceAfter = underlying.balanceOf(wallet2);
+        uint psmUnderlyingBalanceAfter = underlying.balanceOf(address(psm));
 
         assertEq(swapperSynthBalanceAfter, swapperSynthBalanceBefore - expectedAmountIn);
         assertEq(receiverBalanceAfter, receiverBalanceBefore + amountOut);
-        assertEq(psmETHBalanceAfter, psmETHBalanceBefore - amountOut);
+        assertEq(psmUnderlyingBalanceAfter, psmUnderlyingBalanceBefore - amountOut);
     }
 
     function testSwapToSynthGivenIn() public {
         uint amountIn = 10e18;
         uint expectedAmountOut = amountIn * (BPS_SCALE - TO_SYNTH_FEE) / BPS_SCALE;
 
-        uint swapperETHBalanceBefore = wallet1.balance;
+        uint swapperUnderlyingBalanceBefore = underlying.balanceOf(wallet1);
         uint receiverSynthBalanceBefore = synth.balanceOf(wallet2);
-        uint psmETHBalanceBefore = address(psm).balance;
+        uint psmUnderlyingBalanceBefore = underlying.balanceOf(address(psm));
 
         vm.prank(wallet1);
-        psm.swapToSynthGivenIn{value: amountIn}(wallet2);
+        psm.swapToSynthGivenIn(amountIn, wallet2);
 
-        uint swapperETHBalanceAfter = wallet1.balance;
+        uint swapperUnderlyingBalanceAfter = underlying.balanceOf(wallet1);
         uint receiverSynthBalanceAfter = synth.balanceOf(wallet2);
-        uint psmETHBalanceAfter = address(psm).balance;
+        uint psmUnderlyingBalanceAfter = underlying.balanceOf(address(psm));
 
-        assertEq(swapperETHBalanceAfter, swapperETHBalanceBefore - amountIn);
+        assertEq(swapperUnderlyingBalanceAfter, swapperUnderlyingBalanceBefore - amountIn);
         assertEq(receiverSynthBalanceAfter, receiverSynthBalanceBefore + expectedAmountOut);
-        assertEq(psmETHBalanceAfter, psmETHBalanceBefore + amountIn);
+        assertEq(psmUnderlyingBalanceAfter, psmUnderlyingBalanceBefore + amountIn);
     }
 
     function testSwapToSynthGivenOut() public {
         uint amountOut = 10e18;
         uint expectedAmountIn = amountOut * BPS_SCALE / (BPS_SCALE - TO_SYNTH_FEE);
 
-        uint swapperETHBalanceBefore = wallet1.balance;
+        uint swapperUnderlyingBalanceBefore = underlying.balanceOf(wallet1);
         uint receiverSynthBalanceBefore = synth.balanceOf(wallet2);
-        uint psmETHBalanceBefore = address(psm).balance;
+        uint psmUnderlyingBalanceBefore = underlying.balanceOf(address(psm));
 
         vm.prank(wallet1);
-        psm.swapToSynthGivenOut{value: expectedAmountIn}(amountOut, wallet2);
+        psm.swapToSynthGivenOut(amountOut, wallet2);
 
-        uint swapperETHBalanceAfter = wallet1.balance;
+        uint swapperUnderlyingBalanceAfter = underlying.balanceOf(wallet1);
         uint receiverSynthBalanceAfter = synth.balanceOf(wallet2);
-        uint psmETHBalanceAfter = address(psm).balance;
+        uint psmUnderlyingBalanceAfter = underlying.balanceOf(address(psm));
 
-        assertEq(swapperETHBalanceAfter, swapperETHBalanceBefore - expectedAmountIn);
+        assertEq(swapperUnderlyingBalanceAfter, swapperUnderlyingBalanceBefore - expectedAmountIn);
         assertEq(receiverSynthBalanceAfter, receiverSynthBalanceBefore + amountOut);
-        assertEq(psmETHBalanceAfter, psmETHBalanceBefore + expectedAmountIn);
-    }
-
-    function testSwapToSynthGivenOutToLittleETHSendShouldFail() public {
-        uint amountOut = 10e18;
-        uint expectedAmountIn = amountOut * BPS_SCALE / (BPS_SCALE - TO_SYNTH_FEE);
-        
-        vm.startPrank(wallet1);
-        vm.expectRevert(stdError.arithmeticError);
-        psm.swapToSynthGivenOut{value: expectedAmountIn - 10}(amountOut, wallet2);
-        vm.stopPrank();
+        assertEq(psmUnderlyingBalanceAfter, psmUnderlyingBalanceBefore + expectedAmountIn);
     }
 
     // Test quotes
