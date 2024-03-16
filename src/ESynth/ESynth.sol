8c420faa094d6eeadab5915337524a71b3b4db01
Mick de Graaf <mick@mickdegraaf.nl>
2024-03-16 17:26:04 +0100

    TotalSupply Overwrites

--- src/ESynth/ESynth.sol
+++ src/ESynth/ESynth.sol
@@ -3,32 +3,35 @@
 pragma solidity ^0.8.0;
 
 import {Ownable} from "openzeppelin/access/Ownable.sol";
+import {EnumerableSet} from "openzeppelin/utils/structs/EnumerableSet.sol"; 
 import {ERC20Collateral, ERC20, Context} from "../ERC20Collateral/ERC20Collateral.sol";
 import {IEVC, EVCUtil} from "ethereum-vault-connector/utils/EVCUtil.sol";
 import {IEVault} from "../EVault/IEVault.sol";
 
 contract ESynth is ERC20Collateral, Ownable {
-    constructor(IEVC evc_, string memory name_, string memory symbol_)
-        ERC20Collateral(evc_, name_, symbol_)
-        Ownable(msg.sender)
-    {}
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    uint256 public constant MAX_IGNORED = 30;
 
     struct MinterData {
         uint128 capacity;
         uint128 minted;
     }
 
-    error E_CapacityReached();
+    mapping(address => MinterData) public minters;
+    EnumerableSet.AddressSet internal ignoredForTotalSupply;
 
     event MinterCapacitySet(address indexed minter, uint256 capacity);
 
-    mapping(address => MinterData) public minters;
+    error MaxIgnoredExceeded();
+    error E_CapacityReached();
 
-    // @notice Sets the minting capacity for a minter.
+    constructor(IEVC evc_, string memory name_, string memory symbol_) ERC20Collateral(evc_, name_, symbol_) Ownable(msg.sender) {}
+
+    /// @notice Sets the minting capacity for a minter.
     /// @dev Can only be called by the owner of the contract.
     /// @param minter The address of the minter to set the capacity for.
     /// @param capacity The capacity to set for the minter.
-
     function setCapacity(address minter, uint128 capacity) external onlyOwner {
         minters[minter].capacity = capacity;
         emit MinterCapacitySet(minter, capacity);
@@ -95,4 +98,47 @@ contract ESynth is ERC20Collateral, Ownable {
     function _msgSender() internal view virtual override (ERC20Collateral, Context) returns (address) {
         return ERC20Collateral._msgSender();
     }
+
+    // -------- TotalSupply Management --------
+
+    /// @notice Adds an account to the list of accounts to ignore for the total supply.
+    /// @param account The account to add to the list.
+    /// @return success True when the account was not on the list and was added. False otherwise.
+    function addIgnoredForTotalSupply(address account) external onlyOwner returns(bool success) {
+        if(ignoredForTotalSupply.length() >= MAX_IGNORED) {
+            revert MaxIgnoredExceeded();
+        }
+        return ignoredForTotalSupply.add(account);
+    }
+
+    /// @notice Removes an account from the list of accounts to ignore for the total supply.
+    /// @param account The account to remove from the list.
+    /// @return success True when the account was on the list and was removed. False otherwise.
+    function removeIgnoredForTotalSupply(address account) external onlyOwner returns(bool success) {
+        return ignoredForTotalSupply.remove(account);
+    }
+
+    /// @notice Checks if an account is ignored for the total supply.
+    /// @param account The account to check.
+    function isIgnoredForTotalSupply(address account) public view returns (bool) {
+        return ignoredForTotalSupply.contains(account);
+    }
+
+    /// @notice Retrieves all the accounts ignored for the total supply.
+    /// @return The list of accounts ignored for the total supply.
+    function getAllIgnoredForTotalSupply() public view returns (address[] memory) {
+        return ignoredForTotalSupply.values();
+    }
+
+    /// @notice Retrieves the total supply of the token.
+    /// @dev Overriden to exclude the ignored accounts from the total supply.
+    /// @return The total supply of the token.
+    function totalSupply() public view override returns (uint256) {
+        uint256 total = super.totalSupply();
+        for(uint256 i = 0; i < ignoredForTotalSupply.length(); i++) {
+            total -= balanceOf(ignoredForTotalSupply.at(i));
+        }
+        return total;
+    }
+
 }
