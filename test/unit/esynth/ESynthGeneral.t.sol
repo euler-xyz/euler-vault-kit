d2a0b7e4b69419777faf51f50a0d9a251f8ea735
Rangel Stoilov <rangelstoilov@gmail.com>
2024-03-17 10:44:39 +0200

    test: initial testing

---
+++ test/unit/esynth/ESynthGeneral.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {ESynthTest} from "./lib/ESynthTest.sol";
+
+contract ESynthGeneralTest is ESynthTest {
+    function testFuzz_mintShouldIncreaseTotalSupplyAndBalance(uint256 amount) public {
+        amount = bound(amount, 0, type(uint256).max);
+        uint256 balanceBefore = esynth.balanceOf(user1);
+        uint256 totalSupplyBefore = esynth.totalSupply();
+        esynth.mint(user1, amount);
+        assertEq(esynth.balanceOf(user1), balanceBefore + amount);
+        assertEq(esynth.totalSupply(), totalSupplyBefore + amount);
+    }
+
+    function testFuzz_burnShouldDecreaseTotalSupplyAndBalance(uint256 initialAmount, uint256 burnAmount) public {
+        initialAmount = bound(initialAmount, 0, type(uint256).max);
+        esynth.mint(user1, initialAmount);
+        burnAmount = bound(burnAmount, 0, initialAmount);
+
+        vm.startPrank(user1);
+        esynth.approve(address(this), burnAmount);
+        vm.stopPrank();
+
+        uint256 balanceBefore = esynth.balanceOf(user1);
+        uint256 totalSupplyBefore = esynth.totalSupply();
+        esynth.burn(user1, burnAmount);
+
+        assertEq(esynth.balanceOf(user1), balanceBefore - burnAmount);
+        assertEq(esynth.totalSupply(), totalSupplyBefore - burnAmount);
+    }
+}
---
+++ test/unit/esynth/lib/ESynthTest.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import {EthereumVaultConnector as EVC} from "ethereum-vault-connector/EthereumVaultConnector.sol";
+import {ESynth} from "src/ESynth/Esynth.sol";
+import {ESVault} from "src/ESVault/ESVault.sol";
+import {Initialize} from "src/EVault/modules/Initialize.sol";
+import {Token} from "src/EVault/modules/Token.sol";
+import {Vault} from "src/EVault/modules/Vault.sol";
+import {Borrowing} from "src/EVault/modules/Borrowing.sol";
+import {Liquidation} from "src/EVault/modules/Liquidation.sol";
+import {BalanceForwarder} from "src/EVault/modules/BalanceForwarder.sol";
+import {Governance} from "src/EVault/modules/Governance.sol";
+import {RiskManager} from "src/EVault/modules/RiskManager.sol";
+
+import {DeployPermit2} from "permit2/test/utils/DeployPermit2.sol";
+
+import {ProtocolConfig} from "src/ProtocolConfig/ProtocolConfig.sol";
+import {MockPriceOracle} from "../../../mocks/MockPriceOracle.sol";
+import {MockBalanceTracker} from "../../../mocks/MockBalanceTracker.sol";
+import {TypesLib} from "src/EVault/shared/types/Types.sol";
+import {Base} from "src/EVault/shared/Base.sol";
+
+contract ESynthTest is Test, DeployPermit2 {
+    EVC public evc;
+    ESynth public esynth;
+    ESVault public esvault;
+    address admin;
+    address feeReceiver;
+    ProtocolConfig protocolConfig;
+    address balanceTracker;
+    MockPriceOracle oracle;
+    address permit2;
+    address unitOfAccount;
+    address user1;
+    address user2;
+
+    function setUp() public {
+        evc = new EVC();
+        esynth = new ESynth(evc, "SynthTest", "sTST");
+
+        user1 = makeAddr("user1");
+        user2 = makeAddr("user2");
+
+        admin = vm.addr(1000);
+        feeReceiver = makeAddr("feeReceiver");
+
+        protocolConfig = new ProtocolConfig(admin, feeReceiver);
+        balanceTracker = address(new MockBalanceTracker());
+        oracle = new MockPriceOracle();
+        unitOfAccount = address(1);
+        permit2 = deployPermit2();
+
+        Base.Integrations memory integrations = Base.Integrations(address(evc), address(protocolConfig), balanceTracker, permit2);
+        address initializeModule = address(new Initialize(integrations));
+        address tokenModule = address(new Token(integrations));
+        address vaultModule = address(new Vault(integrations));
+        address borrowingModule = address(new Borrowing(integrations));
+        address liquidationModule = address(new Liquidation(integrations));
+        address riskManagerModule = address(new RiskManager(integrations));
+        address balanceForwarderModule = address(new BalanceForwarder(integrations));
+        address governanceModule = address(new Governance(integrations));
+    }
+}
