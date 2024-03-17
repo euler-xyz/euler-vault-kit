bea1a91f3aafd881c21e936845f157ab6dcfc03b
Mick de Graaf <mick@mickdegraaf.nl>
2024-03-17 11:55:17 +0100

    ESVault test base + synth deposit override test

---
+++ test/unit/esvault/ESVault.deposit.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity ^0.8.0;
+
+import {ESVaultTestBase, ESynth} from "./ESVaultTestBase.t.sol";
+import {Errors} from "../../../src/Evault/shared/Errors.sol";
+import {console2} from "forge-std/Test.sol";
+
+contract ESVaultTestDeposit is ESVaultTestBase {
+
+    function setUp() public override {
+        super.setUp();
+    }
+
+    function test_deposit_from_non_synth() public {
+        vm.expectRevert(Errors.E_Unauthorized.selector);
+        eTST.deposit(100, address(this));
+    }
+
+    function test_deposit_from_synth() public {
+        assetTSTAsSynth.mint(address(assetTSTAsSynth), 100);
+        assetTSTAsSynth.deposit(address(eTST), 100);
+
+        assertEq(assetTST.balanceOf(address(eTST)), 100);
+        assertEq(eTST.balanceOf(address(assetTST)), 100);
+    }
+}
---
+++ test/unit/esvault/ESVaultTestBase.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity ^0.8.0;
+
+import {EVaultTestBase} from "../evault/EVaultTestBase.t.sol";
+import {ESVault} from "../../../src/ESVault/ESVault.sol";
+import {IEVault, IERC20} from "../../../src/EVault/IEVault.sol";
+import {IRMTestDefault} from "../../mocks/IRMTestDefault.sol";
+import {ESynth} from "../../../src/ESynth/ESynth.sol";
+import {TestERC20} from "../../mocks/TestERC20.sol";
+
+contract ESVaultTestBase is EVaultTestBase {
+
+    ESynth assetTSTAsSynth;
+    ESynth assetTST2AsSynth;
+
+    function setUp() public virtual override {
+        super.setUp();
+
+        address esVaultImpl = address(
+            new ESVault(
+                integrations,
+                modules
+            )
+        );
+
+        vm.prank(admin);
+        factory.setImplementation(esVaultImpl);
+
+        assetTSTAsSynth = ESynth(address(new ESynth(evc, "Test Synth", "TST")));
+        assetTST = TestERC20(address(assetTSTAsSynth));
+        assetTST2AsSynth = ESynth(address(new ESynth(evc, "Test Synth 2", "TST2")));
+        assetTST2 = TestERC20(address(assetTST2AsSynth));
+
+        eTST = IEVault(factory.createProxy(true, abi.encodePacked(address(assetTST), address(oracle), unitOfAccount)));
+        eTST.setIRM(address(new IRMTestDefault()));
+        // Set the capacity for the vault on the synth
+        // assetTSTAsSynth.setCapacity(address(eTST), type(uint128).max);
+
+        eTST2 = IEVault(factory.createProxy(true, abi.encodePacked(address(assetTST2), address(oracle), unitOfAccount)));
+        eTST2.setIRM(address(new IRMTestDefault()));
+        // Set the capacity for the vault on the synth
+        // assetTST2AsSynth.setCapacity(address(eTST2), type(uint128).max);
+    }
+}
