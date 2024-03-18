05d48a14240871027c4e1965408ec95ef32002a5
Mick de Graaf <mick@mickdegraaf.nl>
2024-03-18 17:37:22 +0100

    remove console import

--- src/ESVault/ESVault.sol
+++ src/ESVault/ESVault.sol
@@ -10,7 +10,6 @@ import {GovernanceModule} from "../EVault/modules/Governance.sol";
 import {IERC20} from "../EVault/IEVault.sol";
 import {ProxyUtils} from "../EVault/shared/lib/ProxyUtils.sol";
 import {Operations} from "../EVault/shared/types/Types.sol";
-import {console2} from "forge-std/Test.sol";
 import "../EVault/shared/Constants.sol";
 import "../EVault/shared/types/Types.sol";
 
