ceed9b641149aeda303a21e958d26f0e71241943
Doug Hoyte <doug@hcsw.org>
2024-03-22 08:38:41 -0400

    document behaviour when multiple flags passed

--- src/EVault/shared/types/Flags.sol
+++ src/EVault/shared/types/Flags.sol
@@ -5,10 +5,12 @@ pragma solidity ^0.8.0;
 import {Flags} from "./Types.sol";
 
 library FlagsLib {
+    /// @dev Are *all* of the flags in bitMask set?
     function isSet(Flags self, uint32 bitMask) internal pure returns (bool) {
-        return (Flags.unwrap(self) & bitMask) != 0;
+        return (Flags.unwrap(self) & bitMask) == bitMask;
     }
 
+    /// @dev Are *none* of the flags in bitMask set?
     function isNotSet(Flags self, uint32 bitMask) internal pure returns (bool) {
         return (Flags.unwrap(self) & bitMask) == 0;
     }
