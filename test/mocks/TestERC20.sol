393df2ad773d7ec463e11e9ad25e3cfbbdda4e9b
kasperpawlowski <25374117+kasperpawlowski@users.noreply.github.com>
2024-03-22 17:30:44 +0000

    fix: remove redundant function

--- test/mocks/TestERC20.sol
+++ test/mocks/TestERC20.sol
@@ -22,7 +22,6 @@ pragma solidity ^0.8.0;
  *     name/return-bytes32                                                     Returns bytes32 instead of string
  *     symbol/return-bytes32                                                   Returns bytes32 instead of string
  *     permit/allowed                                                          Switch permit type to DAI-like 'allowed'
- *     evc/address                             address addr                    EVC() function returns address
  */
 contract TestERC20 {
     address owner;
@@ -314,11 +313,5 @@ contract TestERC20 {
 
     function testExcludeFromCoverage() public pure {
         return;
-    }
-
-    function EVC() external view returns (address) {
-        (bool isSet, bytes memory data) = behaviour("evc/address");
-        if (isSet) return abi.decode(data, (address));
-        revert("no such function");
     }
 }
