a2f23b92427180cb71128e032cabaddca718b830
dglowinski <dariushg32111@gmail.com>
2024-03-24 15:29:25 +0100

    formatting

--- test/unit/productLines/productLines.base.t.sol
+++ test/unit/productLines/productLines.base.t.sol
@@ -9,7 +9,7 @@ contract ProductLine_Base is EVaultTestBase {
         assertEq(coreProductLine.vaultLookup(address(eTST)), true);
         assertEq(coreProductLine.vaultLookup(vm.addr(100)), false);
         assertEq(coreProductLine.getVaultListLength(), 2);
-        assertEq(coreProductLine.getVaultListSlice(0, type(uint).max)[0], address(eTST));
-        assertEq(coreProductLine.getVaultListSlice(0, type(uint).max)[1], address(eTST2));
+        assertEq(coreProductLine.getVaultListSlice(0, type(uint256).max)[0], address(eTST));
+        assertEq(coreProductLine.getVaultListSlice(0, type(uint256).max)[1], address(eTST2));
     }
 }
--- test/unit/productLines/productLines.escrow.t.sol
+++ test/unit/productLines/productLines.escrow.t.sol
@@ -6,8 +6,8 @@ import "src/ProductLines/Escrow.sol";
 import "../evault/EVaultTestBase.t.sol";
 
 contract ProductLine_Escrow is EVaultTestBase {
-    uint32 constant ESCROW_DISABLED_OPS = OP_BORROW | OP_REPAY | OP_LOOP | OP_DELOOP | OP_PULL_DEBT | OP_CONVERT_FEES | OP_LIQUIDATE | OP_TOUCH
-                | OP_ACCRUE_INTEREST;
+    uint32 constant ESCROW_DISABLED_OPS = OP_BORROW | OP_REPAY | OP_LOOP | OP_DELOOP | OP_PULL_DEBT | OP_CONVERT_FEES
+        | OP_LIQUIDATE | OP_TOUCH | OP_ACCRUE_INTEREST;
 
     function test_ProductLine_Escrow_basicViews() public {
         IEVault escrowTST = IEVault(escrowProductLine.createVault(address(assetTST)));
