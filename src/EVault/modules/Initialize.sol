euler-xyz/evk-cantina-fixes

feat: remove CFG_ONLY_ASSET_CAN_DEPOSIT, disabled ops, locked ops and pause guardian; add hooked ops

src/EVault/modules/Initialize.sol
@@ -7,7 +7,6 @@ import {Base} from "../shared/Base.sol";
 import {BorrowUtils} from "../shared/BorrowUtils.sol";
 import {DToken} from "../DToken.sol";
 import {ProxyUtils} from "../shared/lib/ProxyUtils.sol";
-import {RevertBytes} from "../shared/lib/RevertBytes.sol";
 import {MarketCache} from "../shared/types/MarketCache.sol";
 
 import "../shared/Constants.sol";
@@ -42,7 +41,7 @@ abstract contract InitializeModule is IInitialize, Base, BorrowUtils {
         marketStorage.lastInterestAccumulatorUpdate = uint48(block.timestamp);
         marketStorage.interestAccumulator = INITIAL_INTEREST_ACCUMULATOR;
         marketStorage.interestFee = DEFAULT_INTEREST_FEE.toConfigAmount();
-        marketStorage.creator = marketStorage.governorAdmin = marketStorage.pauseGuardian = proxyCreator;
+        marketStorage.creator = marketStorage.governorAdmin = proxyCreator;
 
         snapshot.reset();
 

src/EVault/shared/Base.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import {EVCClient} from "./EVCClient.sol";
 import {Cache} from "./Cache.sol";
+import {RevertBytes} from "./lib/RevertBytes.sol";
 
 import {IProtocolConfig} from "../../ProtocolConfig/IProtocolConfig.sol";
 import {IBalanceTracker} from "../../interfaces/IBalanceTracker.sol";
@@ -54,10 +55,10 @@ abstract contract Base is EVCClient, Cache {
         returns (MarketCache memory marketCache, address account)
     {
         marketCache = updateMarket();
+        account = EVCAuthenticateDeferred(~CONTROLLER_REQUIRED_OPS & operation == 0);
 
-        if (marketCache.disabledOps.isSet(operation)) {
-            revert E_OperationDisabled();
-        }
+        validateOperation(marketCache, operation, account);
+        EVCRequireStatusChecks(accountToCheck == CHECKACCOUNT_CALLER ? account : accountToCheck);
 
         // The snapshot is used only to verify that supply increased when checking the supply cap, and to verify that the borrows
         // increased when checking the borrowing cap. Caps are not checked when the capped variables decrease (become safer).
@@ -69,10 +70,22 @@ abstract contract Base is EVCClient, Cache {
             marketStorage.snapshotInitialized = marketCache.snapshotInitialized = true;
             snapshot.set(marketCache.cash, marketCache.totalBorrows.toAssetsUp());
         }
+    }
 
-        account = EVCAuthenticateDeferred(~CONTROLLER_REQUIRED_OPS & operation == 0);
+    // Checks whether the operation is hookable and if so, calls the hook target.
+    function validateOperation(MarketCache memory marketCache, uint32 operation, address caller) internal {
+        if (marketCache.hookedOps.isNotSet(operation)) return;
 
-        EVCRequireStatusChecks(accountToCheck == CHECKACCOUNT_CALLER ? account : accountToCheck);
+        address hookTarget = marketStorage.hookTarget;
+        (bool success, bytes memory data) = hookTarget.call(abi.encodePacked(msg.data, caller));
+
+        if (!success) {
+            RevertBytes.revertBytes(data);
+        }
+
+        if (hookTarget.code.length == 0) {
+            revert E_OperationDisabled();
+        }
     }
 
     function logMarketStatus(MarketCache memory a, uint256 interestRate) internal {

euler-xyz/euler-vault-kit

src/EVault/modules/Initialize.sol
@@ -7,7 +7,6 @@ import {Base} from "../shared/Base.sol";
 import {BorrowUtils} from "../shared/BorrowUtils.sol";
 import {DToken} from "../DToken.sol";
 import {ProxyUtils} from "../shared/lib/ProxyUtils.sol";
-import {RevertBytes} from "../shared/lib/RevertBytes.sol";
 import {VaultCache} from "../shared/types/VaultCache.sol";
 
 import "../shared/Constants.sol";
@@ -42,7 +41,7 @@ abstract contract InitializeModule is IInitialize, Base, BorrowUtils {
         vaultStorage.lastInterestAccumulatorUpdate = uint48(block.timestamp);
         vaultStorage.interestAccumulator = INITIAL_INTEREST_ACCUMULATOR;
         vaultStorage.interestFee = DEFAULT_INTEREST_FEE.toConfigAmount();
-        vaultStorage.creator = vaultStorage.governorAdmin = vaultStorage.pauseGuardian = proxyCreator;
+        vaultStorage.creator = vaultStorage.governorAdmin = proxyCreator;
 
         snapshot.reset();
 

src/EVault/shared/Base.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import {EVCClient} from "./EVCClient.sol";
 import {Cache} from "./Cache.sol";
+import {RevertBytes} from "./lib/RevertBytes.sol";
 
 import {IProtocolConfig} from "../../ProtocolConfig/IProtocolConfig.sol";
 import {IBalanceTracker} from "../../interfaces/IBalanceTracker.sol";
@@ -54,10 +55,10 @@ abstract contract Base is EVCClient, Cache {
         returns (VaultCache memory vaultCache, address account)
     {
         vaultCache = updateVault();
+        account = EVCAuthenticateDeferred(CONTROLLER_NEUTRAL_OPS & operation == 0);
 
-        if (vaultCache.disabledOps.isSet(operation)) {
-            revert E_OperationDisabled();
-        }
+        validateOperation(vaultCache, operation, account);
+        EVCRequireStatusChecks(accountToCheck == CHECKACCOUNT_CALLER ? account : accountToCheck);
 
         // The snapshot is used only to verify that supply increased when checking the supply cap, and to verify that the borrows
         // increased when checking the borrowing cap. Caps are not checked when the capped variables decrease (become safer).
@@ -69,10 +70,22 @@ abstract contract Base is EVCClient, Cache {
             vaultStorage.snapshotInitialized = vaultCache.snapshotInitialized = true;
             snapshot.set(vaultCache.cash, vaultCache.totalBorrows.toAssetsUp());
         }
+    }
 
-        account = EVCAuthenticateDeferred(CONTROLLER_NEUTRAL_OPS & operation == 0);
+    // Checks whether the operation is hookable and if so, calls the hook target.
+    function validateOperation(VaultCache memory vaultCache, uint32 operation, address caller) internal {
+        if (vaultCache.hookedOps.isNotSet(operation)) return;
 
-        EVCRequireStatusChecks(accountToCheck == CHECKACCOUNT_CALLER ? account : accountToCheck);
+        address hookTarget = vaultStorage.hookTarget;
+        (bool success, bytes memory data) = hookTarget.call(abi.encodePacked(msg.data, caller));
+
+        if (!success) {
+            RevertBytes.revertBytes(data);
+        }
+
+        if (hookTarget.code.length == 0) {
+            revert E_OperationDisabled();
+        }
     }
 
     function logVaultStatus(VaultCache memory a, uint256 interestRate) internal {