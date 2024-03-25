4793f0236abb8874cd73517bdbd55c7934da1f45
Andrew Ferraiuolo <andrew@certora.com>
2024-03-25 16:09:33 +0000

    liquidation mustRevert cases

--- certora/harness/ConstHarness.sol
+++ certora/harness/ConstHarness.sol
@@ -5,4 +5,7 @@ import "../../src/EVault/shared/Constants.sol";
 
 // An empty contract to verify constant properties independently
 contract ConstHarness {
+    // Constants undeclared. Circular dependency if dropped.
+    uint32 public constant OP_DEPOSIT_ = OP_DEPOSIT;
+    uint32 public constant OP_MINT_ = OP_MINT;
 }
