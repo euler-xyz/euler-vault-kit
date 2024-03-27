8783ba0b1cd848b7da5a526782f105071f0aabe7
Andrew Ferraiuolo <andrew@certora.com>
2024-03-27 13:43:02 +0000

    Prove reverting for calculateLiquidation Temporarily give up on trying to prove the same for liquidate...

--- src/EVault/modules/Liquidation.sol
+++ src/EVault/modules/Liquidation.sol
@@ -62,7 +62,8 @@ abstract contract LiquidationModule is ILiquidation, BalanceUtils, LiquidityUtil
         address violator,
         address collateral,
         uint256 desiredRepay
-    ) private view returns (LiquidationCache memory liqCache) {
+    // Munged to internal by certora
+    ) internal view returns (LiquidationCache memory liqCache) {
         // Init cache
 
         liqCache.liquidator = liquidator;
