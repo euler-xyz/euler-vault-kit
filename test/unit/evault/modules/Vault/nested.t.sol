afedea385246c2e44fdf0e0e9e3bd4ce021eab7a
Lee <lee@euler.xyz>
2024-04-03 20:27:15 -0700

    added legacy test cases

--- test/unit/evault/modules/Vault/nested.t.sol
+++ test/unit/evault/modules/Vault/nested.t.sol
@@ -9,9 +9,10 @@ import {IRMMax} from "../../../../mocks/IRMMax.sol";
 
 import {IEVault, IRMTestDefault} from "../../EVaultTestBase.t.sol";
 
+import "forge-std/console2.sol";
+
 import "src/EVault/shared/types/Types.sol";
 import "src/EVault/shared/Constants.sol";
-
 
 contract VaultTest_Nested is EVaultTestBase {
     using TypesLib for uint256;
@@ -37,7 +38,7 @@ contract VaultTest_Nested is EVaultTestBase {
         oracle.setPrice(address(eTST2), unitOfAccount, 1e18);
         eTST.setLTV(address(eTST2), 0.9e4, 0);
 
-        eTSTNested.setLTV(address(eTST2), 0.9e4, 0);
+        eTSTNested.setLTV(address(eTST2), 0.8e4, 0);
 
         // Depositor
 
@@ -54,11 +55,10 @@ contract VaultTest_Nested is EVaultTestBase {
 
         eTST.approve(address(eTSTNested), type(uint256).max);
         eTSTNested.deposit(10e18, depositor);
-        
+
         assertEq(eTST.balanceOf(depositor), 90e18);
         assertEq(eTSTNested.balanceOf(depositor), 10e18);
     }
-
 
     function test_basicBorrow() public {
         startHoax(depositor);
@@ -66,7 +66,6 @@ contract VaultTest_Nested is EVaultTestBase {
         eTST.approve(address(eTSTNested), type(uint256).max);
         eTSTNested.deposit(10e18, depositor);
         vm.stopPrank();
-
 
         startHoax(borrower);
 
@@ -80,7 +79,6 @@ contract VaultTest_Nested is EVaultTestBase {
         eTSTNested.borrow(5e18, borrower);
         assertEq(eTST.balanceOf(borrower), 5e18);
     }
-
 
     function test_basicBorrowAndRedeem() public {
         startHoax(depositor);
@@ -125,10 +123,10 @@ contract VaultTest_Nested is EVaultTestBase {
         eTSTNested.borrow(5e18, borrower);
 
         vm.stopPrank();
-        
+
         startHoax(depositor);
 
-        // expect this to rever as there is some amount borrowed
+        // expect this to revert as there is some amount borrowed
         vm.expectRevert(Errors.E_InsufficientCash.selector);
         eTSTNested.redeem(10e18, depositor, depositor);
 
@@ -145,12 +143,10 @@ contract VaultTest_Nested is EVaultTestBase {
 
         eTSTDoubleNested.setLTV(address(eTST2), 0.9e4, 0);
 
-
         startHoax(depositor);
 
         eTST.approve(address(eTSTNested), type(uint256).max);
         eTSTNested.deposit(20e18, depositor);
-
 
         eTSTNested.approve(address(eTSTDoubleNested), type(uint256).max);
         eTSTDoubleNested.deposit(15e18, depositor);
@@ -171,8 +167,183 @@ contract VaultTest_Nested is EVaultTestBase {
 
         vm.expectRevert(Errors.E_ControllerDisabled.selector);
         eTSTNested.borrow(5e18, borrower);
-        
+
         eTSTDoubleNested.borrow(5e18, borrower);
         assertEq(eTSTNested.balanceOf(borrower), 5e18);
+    }
+
+    function test_legacyDepositWithdraw() public {
+        startHoax(depositor);
+
+        eTST.approve(address(eTSTNested), type(uint256).max);
+        eTSTNested.deposit(10e18, depositor);
+
+        assertEq(eTSTNested.balanceOf(depositor), 10e18);
+        assertEq(eTST.balanceOf(depositor), 90e18); //slightly different as we minted 100e18 to start
+
+        uint256 maxRedeemAmount = eTSTNested.maxRedeem(depositor);
+        eTSTNested.redeem(maxRedeemAmount, depositor, depositor);
+
+        assertEq(eTSTNested.balanceOf(depositor), 0);
+        assertEq(eTST.balanceOf(depositor), 100e18);
+    }
+
+    function test_legacyBasicBorrowAndRepay() public {
+        startHoax(depositor);
+
+        eTST.approve(address(eTSTNested), type(uint256).max);
+        eTSTNested.deposit(10e18, depositor);
+
+        vm.stopPrank();
+
+        startHoax(borrower);
+
+        // deposit collateral
+        assetTST2.mint(borrower, type(uint256).max);
+        assetTST2.approve(address(eTST2), type(uint256).max);
+        eTST2.deposit(20e18, borrower);
+
+        // try to borrow, fails without controller & collateral enabled
+        vm.expectRevert(Errors.E_ControllerDisabled.selector);
+        eTSTNested.borrow(5e18, borrower);
+
+        evc.enableCollateral(borrower, address(eTST2));
+        evc.enableController(borrower, address(eTSTNested));
+
+        vm.stopPrank();
+        eTSTNested.setLTV(address(eTST2), 0, 0);
+        startHoax(borrower);
+        // try to borrow, fails because LTV is too low
+        vm.expectRevert(Errors.E_AccountLiquidity.selector);
+        eTSTNested.borrow(5e18, borrower);
+
+        vm.stopPrank();
+        eTSTNested.setLTV(address(eTST2), 0.8e4, 0);
+        startHoax(borrower);
+
+        // successful borrow
+        eTSTNested.borrow(5e18, borrower);
+
+        assertEq(eTST.balanceOf(address(eTSTNested)), 5e18);
+        assertEq(eTSTNested.debtOf(borrower), 5e18);
+        assertEq(assetTST.balanceOf(address(eTST)), 100e18);
+
+        vm.stopPrank();
+
+        startHoax(depositor);
+
+        // withdraw from base
+        uint256 maxRedeemAmount = eTST.maxRedeem(depositor);
+        eTST.redeem(maxRedeemAmount, depositor, depositor);
+
+        assertEq(eTST.balanceOf(depositor), 0);
+        assertEq(eTSTNested.debtOf(borrower), 5e18);
+        assertEq(assetTST.balanceOf(address(eTST)), 10e18);
+
+        vm.stopPrank();
+
+        address liquidator = makeAddr("liquidator");
+
+        startHoax(borrower);
+
+        vm.expectRevert(Errors.E_AccountLiquidity.selector);
+        eTST2.transfer(liquidator, 19e18);
+
+        // transfer collateral to liquidator
+        eTST2.transfer(liquidator, 10e18);
+        vm.stopPrank();
+
+        oracle.setPrice(address(assetTST), unitOfAccount, 1.65e18);
+
+        startHoax(liquidator);
+
+        (uint256 collateralValue, uint256 liabilityValue) = eTSTNested.accountLiquidity(borrower, false);
+        assertApproxEqAbs((collateralValue * 1e18) / liabilityValue, 0.969e18, 1e15);
+
+        evc.enableCollateral(liquidator, address(eTST2));
+        evc.enableController(liquidator, address(eTSTNested));
+
+        assetTST2.approve(address(eTST2), type(uint256).max);
+
+        (uint256 maxRepay, uint256 yield) = eTSTNested.checkLiquidation(liquidator, borrower, address(eTST2));
+
+        // liquidate nested liability
+        eTSTNested.liquidate(borrower, address(eTST2), maxRepay, 0);
+
+        assertEq(eTST2.balanceOf(liquidator), yield + 10e18);
+        assertEq(eTSTNested.debtOf(liquidator), maxRepay);
+
+        vm.stopPrank();
+        startHoax(depositor);
+        assetTST.transfer(liquidator, 10e18);
+        vm.stopPrank();
+
+        startHoax(liquidator);
+
+        assetTST.approve(address(eTST), type(uint256).max);
+        eTST.deposit(10e18, liquidator);
+
+        eTST.approve(address(eTSTNested), type(uint256).max);
+        uint256 outstandingDebt = eTSTNested.debtOf(liquidator);
+        assertEq(outstandingDebt, maxRepay);
+
+        // repay outstanding debt
+        eTSTNested.repay(outstandingDebt, liquidator);
+        assertEq(eTSTNested.debtOf(liquidator), 0);
+    }
+
+    function test_legacyRepayWhenNotHealthy() public {
+        startHoax(depositor);
+
+        eTST.approve(address(eTSTNested), type(uint256).max);
+        eTSTNested.deposit(10e18, depositor);
+
+        vm.stopPrank();
+
+        startHoax(borrower);
+
+        assetTST2.mint(borrower, type(uint256).max);
+        assetTST2.approve(address(eTST2), type(uint256).max);
+        eTST2.deposit(10e18, borrower);
+
+        evc.enableCollateral(borrower, address(eTST2));
+        evc.enableController(borrower, address(eTSTNested));
+
+        vm.stopPrank();
+        eTSTNested.setLTV(address(eTST2), 0.8e4, 0);
+        startHoax(borrower);
+        eTSTNested.borrow(5e18, borrower);
+
+        oracle.setPrice(address(assetTST), unitOfAccount, 1.65e18);
+
+        // account unhealthy
+        (uint256 collateralValue, uint256 liabilityValue) = eTSTNested.accountLiquidity(borrower, false);
+        assertApproxEqAbs((collateralValue * 1e18) / liabilityValue, 0.969e18, 1e15);
+
+        eTST.approve(address(eTSTNested), type(uint256).max);
+        eTSTNested.repay(5e18, borrower);
+
+        // repaid borrow, back to healthy
+        (collateralValue, liabilityValue) = eTSTNested.accountLiquidity(borrower, false);
+        assertEq(collateralValue, 8e18);
+        assertEq(liabilityValue, 0);
+
+        eTSTNested.borrow(3e18, borrower);
+        oracle.setPrice(address(assetTST), unitOfAccount, 3e18);
+
+        // account unhealthy again
+        (collateralValue, liabilityValue) = eTSTNested.accountLiquidity(borrower, false);
+        assertApproxEqAbs((collateralValue * 1e18) / liabilityValue, 0.888e18, 1e15);
+
+        // partial repay fails
+        vm.expectRevert(Errors.E_AccountLiquidity.selector);
+        eTSTNested.repay(0.1e18, borrower);
+
+        eTSTNested.repay(3e18, borrower);
+
+        // back to healthy
+        (collateralValue, liabilityValue) = eTSTNested.accountLiquidity(borrower, false);
+        assertEq(collateralValue, 8e18);
+        assertEq(liabilityValue, 0);
     }
 }
