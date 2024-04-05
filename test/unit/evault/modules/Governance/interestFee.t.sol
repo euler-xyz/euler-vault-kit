1bedf980a86c57b43dad595fd447d144ed8b36ff
TanyaRoze <roze.tanya01@gmail.com>
2024-04-05 19:10:50 +0300

    rework interest fees tests

--- test/unit/evault/modules/Governance/interestFee.t.sol
+++ test/unit/evault/modules/Governance/interestFee.t.sol
@@ -7,78 +7,83 @@ import {GovernanceModule} from "src/EVault/modules/Governance.sol";
 
 import "src/EVault/shared/types/Types.sol";
 
-uint16 constant MAX_PROTOCOL_FEE_SHARE = 0.5e4;
-uint16 constant GUARANTEED_INTEREST_FEE_MIN = 0.01e4;
-uint16 constant GUARANTEED_INTEREST_FEE_MAX = 0.5e4;
-
 contract GovernanceTest_InterestFee is EVaultTestBase {
     using TypesLib for uint256;
 
     address depositor;
-    address borrower;
-    address borrower2;
 
     function setUp() public override {
         super.setUp();
 
         depositor = makeAddr("depositor");
-        borrower = makeAddr("borrower");
-        borrower2 = makeAddr("borrower_2");
 
         // Setup
-
         oracle.setPrice(address(assetTST), unitOfAccount, 1e18);
         oracle.setPrice(address(eTST2), unitOfAccount, 1e18);
-
         eTST.setLTV(address(eTST2), 0.9e4, 0);
 
         // Depositor
-
         startHoax(depositor);
-
         assetTST.mint(depositor, type(uint256).max);
         assetTST.approve(address(eTST), type(uint256).max);
-        eTST.deposit(100e18, depositor);
-
-        // Borrower 1
-
-        startHoax(borrower);
-
-        assetTST.mint(borrower, 10e18);
-
-        assetTST2.mint(borrower, type(uint256).max);
-        assetTST2.approve(address(eTST2), type(uint256).max);
-        eTST2.deposit(100e18, borrower);
-
-        // Borrower 2
-
-        startHoax(borrower2);
-
-        assetTST.mint(borrower2, 10e18);
-
-        assetTST2.mint(borrower2, type(uint256).max);
-        assetTST2.approve(address(eTST2), type(uint256).max);
-        eTST2.deposit(100e18, borrower2);
+        eTST.deposit(1e25, depositor);
     }
 
-    function test_feesAccrual() public {
-        startHoax(borrower);
-        evc.enableCollateral(borrower, address(eTST2));
-        evc.enableController(borrower, address(eTST));
+    function testFuzz_feesAccrual(uint256 countBorrowers) public {
+        countBorrowers = bound(countBorrowers, 1, 15);
+        address[] memory borrowers = new address[](countBorrowers);
 
-        eTST.borrow(5e18, borrower);
+        assertEq(eTST.totalBorrows(), 0);
+        assertEq(eTST.accumulatedFees(), 0);
 
-        startHoax(borrower2);
-        evc.enableCollateral(borrower2, address(eTST2));
-        evc.enableController(borrower2, address(eTST));
+        borrow(borrowers, countBorrowers);
 
-        eTST.borrow(6e18, borrower2);
+        uint256 amount = eTST.totalBorrows();
 
-        skip(1000);
+        skip(86400 * 365);
 
-        uint256 accumFees = eTST.interestFee() * (eTST.debtOf(borrower) + eTST.debtOf(borrower2) - 11e18) / (1e4);
+        uint256 totalSupplyBefore = eTST.totalSupply();
+        uint256 accumFees = eTST.interestFee() * (eTST.totalBorrows() - amount) / (1e4);
 
-        assertApproxEqAbs(accumFees, eTST.accumulatedFeesAssets(), 10);
+        repay(borrowers, countBorrowers);
+
+        assertApproxEqAbs(accumFees, eTST.accumulatedFeesAssets(), 1000000);
+
+        eTST.convertFees();
+
+        assertEq(eTST.accumulatedFees(), 0);
+        assertEq(eTST.totalSupply(), totalSupplyBefore);
+    }
+
+    function testFuzz_setInterestFee_feesAccrual(uint256 countBorrowers) public {
+        countBorrowers = bound(countBorrowers, 1, 15);
+        address[] memory borrowers = new address[](countBorrowers);
+
+        assertEq(eTST.totalBorrows(), 0);
+        assertEq(eTST.accumulatedFees(), 0);
+
+        borrow(borrowers, countBorrowers);
+
+        uint16 fee = eTST.interestFee();
+        uint256 totalBorrow = eTST.totalBorrows();
+
+        skip(86400 * 365);
+
+        uint256 accumFees1 = fee * (eTST.totalBorrows() - totalBorrow) / (1e4);
+
+        startHoax(address(this));
+        eTST.setInterestFee(0.7e4);
+
+        assertApproxEqAbs(eTST.accumulatedFeesAssets(), accumFees1, 1000000);
+
+        fee = eTST.interestFee();
+        totalBorrow = eTST.totalBorrows();
+
+        skip(86400 * 365);
+
+        uint256 accumFees2 = fee * (eTST.totalBorrows() - totalBorrow) / (1e4);
+
+        assertApproxEqAbs(eTST.accumulatedFeesAssets(), accumFees1 + accumFees2, 0.01e18);
     }
 
     function test_convertFees_AnyInvoke() public {
@@ -93,41 +98,73 @@ contract GovernanceTest_InterestFee is EVaultTestBase {
 
         startHoax(depositor);
         eTST.convertFees();
-
-        startHoax(borrower);
-        eTST.convertFees();
     }
 
     function test_convertFees_NoGovernorReceiver() public {
+        uint256 countBorrowers = 15;
+        address[] memory borrowers = new address[](countBorrowers);
+
         startHoax(address(this));
         eTST.setFeeReceiver(address(0));
-        assertEq(eTST.feeReceiver(), address(0));
 
+        assertEq(eTST.feeReceiver(), address(0));
+        assertEq(eTST.accumulatedFees(), 0);
+        assertEq(eTST.totalBorrows(), 0);
+
+        borrow(borrowers, countBorrowers);
+        skip(86400 * 100);
+        repay(borrowers, countBorrowers);
+
+        uint256 totalSupplyBefore = eTST.totalSupply();
         uint256 accumFee = eTST.accumulatedFees();
         address protocolFeeReceiver = protocolConfig.feeReceiver();
 
         assertEq(eTST.balanceOf(protocolFeeReceiver), 0);
+        assertEq(eTST.balanceOf(address(0)), 0);
+
         eTST.convertFees();
+
         assertEq(eTST.balanceOf(protocolFeeReceiver), accumFee);
+        assertEq(eTST.balanceOf(address(0)), 0);
+        assertEq(eTST.totalSupply(), totalSupplyBefore);
+        assertEq(eTST.accumulatedFees(), 0);
     }
 
-    function test_convertFees_WithGovernorReceiver() public {
+    function testFuzz_convertFees_WithGovernorReceiver(uint256 countBorrowers) public {
+        countBorrowers = bound(countBorrowers, 1, 15);
+        address[] memory borrowers = new address[](countBorrowers);
+
+        assertEq(eTST.totalBorrows(), 0);
+        assertEq(eTST.accumulatedFees(), 0);
+
+        borrow(borrowers, countBorrowers);
+        skip(86400 * 365);
+        repay(borrowers, countBorrowers);
+
+        uint256 totalSupplyBefore = eTST.totalSupply();
+        uint256 accumFee = eTST.accumulatedFees();
+        uint256 protocolShare = eTST.protocolFeeShare();
+        uint256 partFee = accumFee.toShares().mulDiv(1e4 - protocolShare, 1e4).toUint();
+
         address governFeeReceiver = eTST.feeReceiver();
         address protocolFeeReceiver = protocolConfig.feeReceiver();
 
-        uint256 accumFee = getAccumulatedFees();
-        uint256 protocolShare = eTST.protocolFeeShare();
-        uint256 partFee = accumFee.toShares().mulDiv(1e4 - protocolShare, 1e4).toUint();
         assertEq(eTST.balanceOf(governFeeReceiver), 0);
         assertEq(eTST.balanceOf(protocolFeeReceiver), 0);
+
         eTST.convertFees();
 
         assertEq(eTST.balanceOf(governFeeReceiver), partFee);
         assertEq(eTST.balanceOf(protocolFeeReceiver), accumFee - partFee);
+        assertEq(eTST.totalSupply(), totalSupplyBefore);
+        assertEq(eTST.accumulatedFees(), 0);
     }
 
-    function test_convertFees_OverMaxProtocolFeeShare() public {
-        uint16 newProtocolFeeShare = MAX_PROTOCOL_FEE_SHARE + 0.1e4;
+    function testFuzz_convertFees_OverMaxProtocolFeeShare(uint256 countBorrowers) public {
+        countBorrowers = bound(countBorrowers, 1, 15);
+        address[] memory borrowers = new address[](countBorrowers);
+
+        uint16 newProtocolFeeShare = 0.5e4 + 0.1e4;
 
         address governFeeReceiver = eTST.feeReceiver();
         address protocolFeeReceiver = protocolConfig.feeReceiver();
@@ -135,65 +172,100 @@ contract GovernanceTest_InterestFee is EVaultTestBase {
         startHoax(admin);
         protocolConfig.setProtocolFeeShare(newProtocolFeeShare);
 
-        uint256 accumFee = getAccumulatedFees();
+        assertEq(eTST.totalBorrows(), 0);
+        assertEq(eTST.accumulatedFees(), 0);
+
+        borrow(borrowers, countBorrowers);
+        skip(86400 * 365);
+        repay(borrowers, countBorrowers);
+
+        uint256 totalSupplyBefore = eTST.totalSupply();
+        uint256 accumFee = eTST.accumulatedFees();
 
         assertEq(eTST.balanceOf(governFeeReceiver), 0);
         assertEq(eTST.balanceOf(protocolFeeReceiver), 0);
+
         eTST.convertFees();
 
         uint256 partFee = accumFee.toShares().mulDiv(1e4 - newProtocolFeeShare, 1e4).toUint();
         assertNotEq(eTST.balanceOf(governFeeReceiver), partFee);
         assertNotEq(eTST.balanceOf(protocolFeeReceiver), accumFee - partFee);
 
-        partFee = accumFee.toShares().mulDiv(1e4 - MAX_PROTOCOL_FEE_SHARE, 1e4).toUint();
+        partFee = accumFee.toShares().mulDiv(1e4 - 0.5e4, 1e4).toUint();
         assertEq(eTST.balanceOf(governFeeReceiver), partFee);
         assertEq(eTST.balanceOf(protocolFeeReceiver), accumFee - partFee);
+        assertEq(eTST.totalSupply(), totalSupplyBefore);
+        assertEq(eTST.accumulatedFees(), 0);
     }
 
-    function test_setInterestFee_InsideGuaranteedRange() public {
-        uint16 newInterestFee = 0.3e4;
+    function testFuzz_setInterestFee_InsideGuaranteedRange(uint16 newInterestFee) public {
+        vm.assume(newInterestFee >= 0.1e4 && newInterestFee <= 1e4);
 
         startHoax(address(this));
-
         vm.expectEmit();
         emit GovernanceModule.GovSetInterestFee(newInterestFee);
-
         eTST.setInterestFee(newInterestFee);
-
         assertEq(eTST.interestFee(), newInterestFee);
     }
 
-    function test_setInterestFee_OutsideGuaranteedRange() public {
-        uint16 newInterestFee = 0.05e4;
+    function testFuzz_setInterestFee_OutsideGuaranteedRange(uint16 newInterestFee) public {
+        vm.assume(newInterestFee < 0.1e4 || newInterestFee > 1e4);
 
         startHoax(address(this));
         vm.expectRevert(Errors.E_BadFee.selector);
         eTST.setInterestFee(newInterestFee);
-        startHoax(admin);
-        protocolConfig.setVaultInterestFeeRange(address(eTST), true, 0, 1e4);
+    }
+
+    function testFuzz_setInterestFee_BelowLowerBound(uint16 newInterestFee) public {
+        vm.assume(newInterestFee < 0.1e4 - 1);
 
         startHoax(address(this));
-
-        vm.expectEmit();
-        emit GovernanceModule.GovSetInterestFee(newInterestFee);
-
+        vm.expectRevert(Errors.E_BadFee.selector);
         eTST.setInterestFee(newInterestFee);
 
+        startHoax(admin);
+        protocolConfig.setVaultInterestFeeRange(address(eTST), true, 0.1e4 - 1, 1e4);
+
+        startHoax(address(this));
+        vm.expectRevert(Errors.E_BadFee.selector);
+        eTST.setInterestFee(newInterestFee);
+
+        startHoax(admin);
+        protocolConfig.setVaultInterestFeeRange(address(eTST), true, newInterestFee, 1e4);
+
+        startHoax(address(this));
+        vm.expectEmit();
+        emit GovernanceModule.GovSetInterestFee(newInterestFee);
+        eTST.setInterestFee(newInterestFee);
         assertEq(eTST.interestFee(), newInterestFee);
     }
 
-    function getAccumulatedFees() internal returns (uint256 accumFee) {
-        startHoax(borrower);
-        evc.enableCollateral(borrower, address(eTST2));
-        evc.enableController(borrower, address(eTST));
+    function borrow(address[] memory borrowers, uint256 count) internal {
+        uint256 total;
+        for (uint8 i; i < count; ++i) {
+            uint256 amount = (i + 1) * 1e18;
+            borrowers[i] = vm.addr(i + 200);
 
-        eTST.borrow(5e18, borrower);
+            assetTST.mint(borrowers[i], amount);
+            assetTST2.mint(borrowers[i], amount * 2);
 
-        skip(1000);
+            startHoax(borrowers[i]);
+            assetTST2.approve(address(eTST2), type(uint256).max);
+            eTST2.deposit(type(uint256).max, borrowers[i]);
+            evc.enableCollateral(borrowers[i], address(eTST2));
+            evc.enableController(borrowers[i], address(eTST));
 
-        assetTST.approve(address(eTST), type(uint256).max);
-        eTST.repay(type(uint256).max, borrower);
+            eTST.borrow(amount, borrowers[i]);
+            total += amount;
+        }
+        assertEq(eTST.totalBorrows(), total);
+    }
 
-        return eTST.accumulatedFees();
+    function repay(address[] memory borrowers, uint256 count) internal {
+        for (uint8 i; i < count; ++i) {
+            startHoax(borrowers[i]);
+            assetTST.approve(address(eTST), type(uint256).max);
+            eTST.repay(type(uint256).max, borrowers[i]);
+        }
     }
 }
