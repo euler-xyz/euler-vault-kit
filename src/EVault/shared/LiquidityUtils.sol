8950b4d306dc2620db1dca138fd248609993d94a
kasperpawlowski <25374117+kasperpawlowski@users.noreply.github.com>
2024-04-05 23:50:37 +0200

    add virtual modifier for LiquidityUtils functions

--- src/EVault/shared/LiquidityUtils.sol
+++ src/EVault/shared/LiquidityUtils.sol
@@ -19,7 +19,7 @@ abstract contract LiquidityUtils is BorrowUtils, LTVUtils {
         address account,
         address[] memory collaterals,
         LTVType ltvType
-    ) internal view returns (uint256 collateralValue, uint256 liabilityValue) {
+    ) internal view virtual returns (uint256 collateralValue, uint256 liabilityValue) {
         validateOracle(vaultCache);
 
         for (uint256 i; i < collaterals.length; ++i) {
@@ -33,6 +33,7 @@ abstract contract LiquidityUtils is BorrowUtils, LTVUtils {
     function checkLiquidity(VaultCache memory vaultCache, address account, address[] memory collaterals)
         internal
         view
+        virtual
     {
         validateOracle(vaultCache);
 
@@ -54,7 +55,7 @@ abstract contract LiquidityUtils is BorrowUtils, LTVUtils {
     // If LTV is zero, the collateral can still be liquidated.
     // If the price of collateral is zero, liquidations are not executed, so the check won't be performed.
     // If there is no collateral balance at all, then debt socialization can happen.
-    function checkNoCollateral(address account, address[] memory collaterals) internal view returns (bool) {
+    function checkNoCollateral(address account, address[] memory collaterals) internal view virtual returns (bool) {
         for (uint256 i; i < collaterals.length; ++i) {
             address collateral = collaterals[i];
 
@@ -70,6 +71,7 @@ abstract contract LiquidityUtils is BorrowUtils, LTVUtils {
     function getLiabilityValue(VaultCache memory vaultCache, address account, Owed owed)
         internal
         view
+        virtual
         returns (uint256 value)
     {
         // update owed with interest accrued
@@ -88,6 +90,7 @@ abstract contract LiquidityUtils is BorrowUtils, LTVUtils {
     function getCollateralValue(VaultCache memory vaultCache, address account, address collateral, LTVType ltvType)
         internal
         view
+        virtual
         returns (uint256 value)
     {
         ConfigAmount ltv = getLTV(collateral, ltvType);
