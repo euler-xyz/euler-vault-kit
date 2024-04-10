0e30fd6a1fcca28e8d3a649c8b206a1b8316105e
TanyaRoze <roze.tanya01@gmail.com>
2024-04-10 18:21:21 +0300

    add balances tests

--- test/unit/evault/modules/Vault/borrowBasic.t.sol
+++ test/unit/evault/modules/Vault/borrowBasic.t.sol
@@ -5,9 +5,9 @@ pragma solidity ^0.8.0;
 import {EVaultTestBase} from "../../EVaultTestBase.t.sol";
 import {IRMTestZero} from "../../../../mocks/IRMTestZero.sol";
 import {IRMTestFixed} from "../../../../mocks/IRMTestFixed.sol";
-import "src/EVault/shared/types/Types.sol";
-
 import {Errors} from "src/EVault/shared/Errors.sol";
+import {Events} from "src/EVault/shared/Events.sol";
+import "src/EVault/shared/types/Types.sol";
 
 contract VaultTest_BorrowBasic is EVaultTestBase {
     using TypesLib for uint256;
@@ -24,7 +24,7 @@ contract VaultTest_BorrowBasic is EVaultTestBase {
         assetTST.mint(depositor, 100e18);
         assetTST.mint(borrower, 100e18);
         assetTST2.mint(borrower, 100e18);
-        
+
         startHoax(depositor);
         assetTST.approve(address(eTST), type(uint256).max);
         eTST.deposit(1e18, depositor);
@@ -38,14 +38,13 @@ contract VaultTest_BorrowBasic is EVaultTestBase {
         oracle.setPrice(address(assetTST), unitOfAccount, 0.01e18);
         oracle.setPrice(address(eTST2), unitOfAccount, 0.05e18);
 
-        skip(31*60);
+        skip(31 * 60);
 
         startHoax(address(this));
         eTST.setLTV(address(eTST2), 0.21e4, 0);
     }
 
     function test_borrow_noInterest() public {
-
         eTST.setInterestRateModel(address(new IRMTestZero()));
 
         address[] memory collaterals = evc.getCollaterals(borrower);
@@ -62,14 +61,18 @@ contract VaultTest_BorrowBasic is EVaultTestBase {
         // Liability vault must be the EVC controller
         vm.expectRevert(Errors.E_ControllerDisabled.selector);
         eTST.borrow(0.4e18, borrower);
-        
+
         evc.enableController(borrower, address(eTST));
 
         // Two separate borrows, .4 and .1:
         vm.expectRevert(Errors.E_BadAssetReceiver.selector);
         eTST.borrow(0.5e18, getSubAccount(borrower, 1));
 
-        eTST.borrow(0.5e18, borrower);
+        vm.expectEmit();
+        emit Events.Transfer(address(0), borrower, 0.4e18);
+        eTST.borrow(0.4e18, borrower);
+
+        eTST.borrow(0.1e18, borrower);
 
         // Make sure the borrow market is recorded
         collaterals = evc.getCollaterals(borrower);
@@ -89,7 +92,9 @@ contract VaultTest_BorrowBasic is EVaultTestBase {
 
         // No interest was charged
         assertEq(eTST.debtOf(borrower), 0.5e18);
-        
+
+        vm.expectEmit();
+        emit Events.Transfer(borrower, address(0), 0.5e18);
         eTST.repay(0.5e18, borrower);
 
         assertEq(assetTST.balanceOf(borrower), 100e18);
@@ -105,7 +110,7 @@ contract VaultTest_BorrowBasic is EVaultTestBase {
         assertEq(controllers.length, 0);
     }
 
-    function test_borrow_VerySmallInterest() public {
+    function test_borrow_verySmallInterest() public {
         startHoax(borrower);
         evc.enableController(borrower, address(eTST));
 
@@ -119,7 +124,7 @@ contract VaultTest_BorrowBasic is EVaultTestBase {
         // Mint some extra so we can pay interest
         assetTST.mint(borrower, 0.1e18);
         skip(1);
-        assertEq(eTST.interestAccumulator(), 1.000000003170979198376458650e27);
+        assertEq(eTST.interestAccumulator(), 1.00000000317097919837645865e27);
 
         startHoax(borrower);
         eTST.borrow(0.5e18, borrower);
@@ -128,11 +133,12 @@ contract VaultTest_BorrowBasic is EVaultTestBase {
         skip(1);
         assertEq(eTST.interestAccumulator(), 1.000000006341958406808026376e27);
 
-
         // 1 block later, notice amount owed is rounded up:
-        assertEq(eTST.debtOf(borrower), 0.500000001585489600e18);
-        assertApproxEqAbs(eTST.debtOfExact(borrower), debtExact(0.500000001585489599188229324e27), 0.00000000000000001e18);
-        
+        assertEq(eTST.debtOf(borrower), 0.5000000015854896e18);
+        assertApproxEqAbs(
+            eTST.debtOfExact(borrower), debtExact(0.500000001585489599188229324e27), 0.00000000000000001e18
+        );
+
         // Use max uint to actually pay off full amount:
         eTST.repay(type(uint256).max, borrower);
 
@@ -157,11 +163,11 @@ contract VaultTest_BorrowBasic is EVaultTestBase {
         eTST.setInterestRateModel(address(new IRMTestFixed()));
 
         assertEq(eTST.interestAccumulator(), 1e27);
-            
+
         // Mint some extra so we can pay interest
         assetTST.mint(borrower, 0.1e18);
         skip(1);
-        assertEq(eTST.interestAccumulator(), 1.000000003170979198376458650e27);
+        assertEq(eTST.interestAccumulator(), 1.00000000317097919837645865e27);
 
         startHoax(borrower);
         eTST.borrow(0.5e18, borrower);
@@ -174,9 +180,11 @@ contract VaultTest_BorrowBasic is EVaultTestBase {
         startHoax(address(this));
         eTST.setInterestRateModel(address(new IRMTestZero()));
 
-        assertEq(eTST.debtOf(borrower), 0.500000001585489600e18);
-        assertApproxEqAbs(eTST.debtOfExact(borrower), debtExact(0.500000001585489599188229324e27), 0.00000000000000001e18);
-        
+        assertEq(eTST.debtOf(borrower), 0.5000000015854896e18);
+        assertApproxEqAbs(
+            eTST.debtOfExact(borrower), debtExact(0.500000001585489599188229324e27), 0.00000000000000001e18
+        );
+
         startHoax(borrower);
         eTST.repay(0.500000001585489599e18, borrower);
 
@@ -200,7 +208,7 @@ contract VaultTest_BorrowBasic is EVaultTestBase {
         assertEq(evc.getControllers(borrower).length, 0);
     }
 
-    function test_AmountsAtLimit() public {
+    function test_amountsAtLimit() public {
         eTST.setInterestRateModel(address(new IRMTestZero()));
 
         // Try to borrow more tokens than exist in the pool:
@@ -226,11 +234,11 @@ contract VaultTest_BorrowBasic is EVaultTestBase {
         assertEq(MAX_SANE_AMOUNT.toAssets().toOwed().toUint(), MAX_SANE_DEBT_AMOUNT);
     }
 
-    function debtExact(uint256 value) internal returns(uint256){
+    function debtExact(uint256 value) internal returns (uint256) {
         return value * (2 ** 31) / (10 ** 9);
     }
 
-    function getSubAccount(address primary, uint8 subAccountId) internal returns(address){
+    function getSubAccount(address primary, uint8 subAccountId) internal returns (address) {
         require(subAccountId <= 256, "invalid subAccountId");
         return address(uint160(uint160(primary) ^ subAccountId));
     }
