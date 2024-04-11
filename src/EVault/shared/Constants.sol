5a7f92794843d64f29a59c4b8c7b85e7507e8c8c
dglowinski <dariushg32111@gmail.com>
2024-04-11 19:26:40 +0200

    define CONFIG_SCALE constant

--- src/EVault/shared/Constants.sol
+++ src/EVault/shared/Constants.sol
@@ -15,6 +15,8 @@ uint256 constant PROXY_METADATA_LENGTH = 60;
 uint256 constant SECONDS_PER_YEAR = 365.2425 * 86400;
 // max interest rate accepted from interest rate model contract. 1,000,000% APY: ln(1 + (1000000 / 100)) * 1e27 / (365.2425 * 86400)
 uint256 constant MAX_ALLOWED_INTEREST_RATE = 291867236321699131285;
+// max valid value of the ConfigAmount custom type, signifying 100%
+uint16 constant CONFIG_SCALE = 1e4;
 
 // Account status checks special values
 
--- src/EVault/shared/types/ConfigAmount.sol
+++ src/EVault/shared/types/ConfigAmount.sol
@@ -15,21 +15,21 @@ library ConfigAmountLib {
     // note assuming arithmetic checks are already performed
     function mulDiv(ConfigAmount self, uint256 multiplier, uint256 divisor) internal pure returns (uint256) {
         unchecked {
-            return uint256(self.toUint16()) * multiplier / (1e4 * divisor);
+            return uint256(self.toUint16()) * multiplier / (CONFIG_SCALE * divisor);
         }
     }
 
     // note assuming arithmetic checks are already performed
     function mul(ConfigAmount self, uint256 multiplier) internal pure returns (uint256) {
         unchecked {
-            return uint256(self.toUint16()) * multiplier / 1e4;
+            return uint256(self.toUint16()) * multiplier / CONFIG_SCALE;
         }
     }
 
     // note assuming arithmetic checks are already performed
     function mulInv(ConfigAmount self, uint256 multiplier) internal pure returns (uint256) {
         unchecked {
-            return 1e4 * multiplier / uint256(self.toUint16());
+            return CONFIG_SCALE * multiplier / uint256(self.toUint16());
         }
     }
 
@@ -42,7 +42,7 @@ library ConfigAmountLib {
     }
 
     function validate(uint256 amount) internal pure {
-        if (amount > 1e4) revert Errors.E_InvalidConfigAmount();
+        if (amount > CONFIG_SCALE) revert Errors.E_InvalidConfigAmount();
     }
 }
 
