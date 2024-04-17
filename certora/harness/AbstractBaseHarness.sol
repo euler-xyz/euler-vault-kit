7f07f9e75aef8461ebef4f01744228dfdccf206d
Andrew Ferraiuolo <andrew@certora.com>
2024-04-17 14:41:02 +0100

    More refactoring

--- certora/harness/AbstractBaseHarness.sol
+++ certora/harness/AbstractBaseHarness.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.0;
 
+import {IEVC} from "ethereum-vault-connector/interfaces/IEthereumVaultConnector.sol";
 import "../../src/EVault/shared/Base.sol";
 
 // This mainly exists so that Base.LTVConfig and other type declarations 
@@ -13,7 +14,20 @@ abstract contract AbstractBaseHarness is Base {
         return getCollaterals(account);
     }
 
-    // function getLTVConfig(address collateral) external view returns (LTVConfig memory) {
-    //     return vaultStorage.ltvLookup[collateral];
-    // }
+    function getLTVConfig(address collateral) external view returns (LTVConfig memory) {
+        return vaultStorage.ltvLookup[collateral];
+    }
+
+    function vaultCacheOracleConfigured() external returns (bool) {
+        return address(loadVault().oracle) != address(0);
+    }
+
+    function vaultIsOnlyController(address account) external view returns (bool) {
+        address[] memory controllers = IEVC(evc).getControllers(account);
+        return controllers.length == 1 && controllers[0] == address(this);
+    }
+
+    function vaultIsController(address account) external view returns (bool) {
+        return IEVC(evc).isControllerEnabled(account, address(this));
+    }
 }
