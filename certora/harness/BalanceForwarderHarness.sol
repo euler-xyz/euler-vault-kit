81004bafcbf8f9299684d5be6bc90cd51a37244f
Andrew Ferraiuolo <andrew@certora.com>
2024-04-18 11:45:22 +0100

    More refactoring. Use BaseHarness for BalanceForwarder

--- certora/harness/BalanceForwarderHarness.sol
+++ certora/harness/BalanceForwarderHarness.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
-import "../../src/EVault/modules/RiskManager.sol";
+import "../../src/EVault/modules/BalanceForwarder.sol";
 import "../../src/EVault/shared/types/Types.sol";
 import "../../src/interfaces/IPriceOracle.sol";
 import {IERC20} from "../../src/EVault/IEVault.sol";
 import {ERC20} from "../../lib/ethereum-vault-connector/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "../../certora/harness/AbstractBaseHarness.sol";
 
-contract RiskManagerHarness is RiskManager, AbstractBaseHarness {
-    constructor(Integrations memory integrations) RiskManager(integrations) {}
+contract BalanceForwarderHarness is BalanceForwarder, AbstractBaseHarness {
+    constructor(Integrations memory integrations) BalanceForwarder(integrations) {}
 }
