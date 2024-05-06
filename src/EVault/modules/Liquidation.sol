d4f1606c4dc5047005c4114d4f87c146d214f254
dglowinski <dariushg32111@gmail.com>
2024-05-06 20:36:52 +0200

    explicit visibility of constants and immutables

--- src/EVault/modules/Liquidation.sol
+++ src/EVault/modules/Liquidation.sol
@@ -15,7 +15,7 @@ import "../shared/types/Types.sol";
 abstract contract LiquidationModule is ILiquidation, Base, BalanceUtils, LiquidityUtils {
     using TypesLib for uint256;
 
-    // Maximum liquidation discount that can be awarded under any conditions in wad.
+    // Maximum liquidation discount that can be awarded under any conditions.
     uint256 internal constant MAXIMUM_LIQUIDATION_DISCOUNT = 0.2e18;
 
     struct LiquidationCache {
