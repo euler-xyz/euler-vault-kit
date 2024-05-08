03b47ddcf4eff745c5bd070ca7c73e60f6e4014e
dglowinski <dariushg32111@gmail.com>
2024-05-08 14:44:33 +0200

    fix natspec

--- src/ProtocolConfig/IProtocolConfig.sol
+++ src/ProtocolConfig/IProtocolConfig.sol
@@ -11,6 +11,8 @@ pragma solidity ^0.8.0;
 interface IProtocolConfig {
     /**
      * @notice check if a vault's interest fee is valid
+     * @param vault address of the vault
+     * @param interestFee an interest fee value to check
      * @dev an interest fee is considered valid only when it is greater than or equal the min interest fee and less than or equal the max interest fee
      * @dev if a vault has a specific interest fee ranges set by admin, it will be used, otherwise the generic ones will bech checked against
      * @return bool true for valid, else false
@@ -19,6 +21,7 @@ interface IProtocolConfig {
 
     /**
      * @notice get protocol fee config for a certain vault
+     * @param vault address of the vault
      * @dev if vault == address(0), the generic config will be returned
      * @return address protocol fee receiver
      * @return uint16 protocol fee share
@@ -27,6 +30,7 @@ interface IProtocolConfig {
 
     /**
      * @notice get interest fee ranges for a certain vault
+     * @param vault address of the vault
      * @dev if vault == address(0), the generic ranges will be returned
      * @return uint16 min interest fee
      * @return uint16 max interest fee
