31ea176a8d44f4e4dd77a9edbdbdcb4897697273
dglowinski <dariushg32111@gmail.com>
2024-05-08 12:23:13 +0200

    add missing docstrings

--- src/EVault/DToken.sol
+++ src/EVault/DToken.sol
@@ -11,6 +11,7 @@ import {IERC20, IEVault} from "./IEVault.sol";
 /// @author Euler Labs (https://www.eulerlabs.com/)
 /// @notice Contract implements read only ERC20 interface, and `Transfer` events, for EVault's debt
 contract DToken is IERC20, Errors, Events {
+    /// @notice The address of the EVault associated with this DToken
     address public immutable eVault;
 
     constructor() {
@@ -19,44 +20,59 @@ contract DToken is IERC20, Errors, Events {
 
     // ERC20 interface
 
+    /// @notice The debt token (dToken) name
     function name() external view returns (string memory) {
         return string.concat("Debt token of ", IEVault(eVault).name());
     }
 
+    /// @notice The debt token (dToken) symbol
     function symbol() external view returns (string memory) {
         return string.concat(IEVault(eVault).symbol(), "-DEBT");
     }
 
+    /// @notice Decimals of the dToken, same as EVault's
     function decimals() external view returns (uint8) {
         return IEVault(eVault).decimals();
     }
 
+    /// @notice Total supply of the DToken
     function totalSupply() external view returns (uint256) {
         return IEVault(eVault).totalBorrows();
     }
 
+    /// @notice Balance of a particular account, in dTokens
     function balanceOf(address owner) external view returns (uint256) {
         return IEVault(eVault).debtOf(owner);
     }
 
+    /// @notice Retrieve the current allowance
+    /// @dev Approvals are not supported by the dToken
     function allowance(address, address) external pure returns (uint256) {
         return 0;
     }
 
+    /// @notice Function required by the ERC20 interface
+    /// @dev Approvals are not supported by the DToken
     function approve(address, uint256) external pure returns (bool) {
         revert E_NotSupported();
     }
 
+    /// @notice Function required by the ERC20 interface
+    /// @dev Transfers are not supported by the DToken directly
     function transfer(address, uint256) external pure returns (bool) {
         revert E_NotSupported();
     }
 
+    /// @notice Function required by the ERC20 interface
+    /// @dev Transfers are not supported by the DToken directly
     function transferFrom(address, address, uint256) external pure returns (bool) {
         revert E_NotSupported();
     }
 
     // Events
 
+    /// @notice Emit an ERC20 Transfer event
+    /// @dev Only callable by the parent EVault
     function emitTransfer(address from, address to, uint256 value) external {
         if (msg.sender != eVault) revert E_Unauthorized();
 
@@ -65,6 +81,7 @@ contract DToken is IERC20, Errors, Events {
 
     // Helpers
 
+    /// @notice Return the address of the asset the debt is denominated in
     function asset() external view returns (address) {
         return IEVault(eVault).asset();
     }
--- src/EVault/modules/Borrowing.sol
+++ src/EVault/modules/Borrowing.sol
@@ -14,6 +14,8 @@ import "../shared/types/Types.sol";
 
 /// @notice Definition of callback method that flashLoan will invoke on your contract
 interface IFlashLoan {
+    /// @notice Function that will be called on the caller of `flashloan`
+    /// @param data Data that was passed to the `flashloan` call
     function onFlashLoan(bytes memory data) external;
 }
 
