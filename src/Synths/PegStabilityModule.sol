5c4dc653c66f75f82135117faba5b9212d85b48e
Mick de Graaf <mick@mickdegraaf.nl>
2024-05-08 19:36:32 +0200

    Allow a configurable conversion price
    spearbit-audits/review-euler#133

--- src/Synths/PegStabilityModule.sol
+++ src/Synths/PegStabilityModule.sol
@@ -10,21 +10,40 @@ import {ESynth} from "./ESynth.sol";
 contract PegStabilityModule is EVCUtil {
     using SafeERC20 for IERC20;
 
-    uint256 public constant BPS_SCALE = 10000;
+    uint256 public constant BPS_SCALE = 100_00;
+    uint256 public constant PRICE_SCALE = 1e18;
 
     ESynth public immutable synth;
     IERC20 public immutable underlying;
+    uint256 public immutable conversionPrice; // 1e18 = 1 SYNTH == 1 UNDERLYING, 0.01e18 = 1 SYNTH == 0.01 UNDERLYING
 
     uint256 public immutable TO_UNDERLYING_FEE;
     uint256 public immutable TO_SYNTH_FEE;
 
-    constructor(address _evc, address _synth, address _underlying, uint256 toUnderlyingFeeBPS, uint256 toSynthFeeBPS)
-        EVCUtil(IEVC(_evc))
-    {
+    error E_ZeroAddress();
+    error E_FeeExceedsBPS();
+
+    constructor(
+        address _evc,
+        address _synth,
+        address _underlying,
+        uint256 toUnderlyingFeeBPS,
+        uint256 toSynthFeeBPS,
+        uint256 _conversionPrice
+    ) EVCUtil(_evc) {
+        if (toUnderlyingFeeBPS > BPS_SCALE || toSynthFeeBPS > BPS_SCALE) {
+            revert E_FeeExceedsBPS();
+        }
+
+        if (_evc == address(0) || _synth == address(0) || _underlying == address(0)) {
+            revert E_ZeroAddress();
+        }
+
         synth = ESynth(_synth);
         underlying = IERC20(_underlying);
         TO_UNDERLYING_FEE = toUnderlyingFeeBPS;
         TO_SYNTH_FEE = toSynthFeeBPS;
+        conversionPrice = _conversionPrice;
     }
 
     function swapToUnderlyingGivenIn(uint256 amountIn, address receiver) external returns (uint256) {
@@ -64,18 +83,18 @@ contract PegStabilityModule is EVCUtil {
     }
 
     function quoteToUnderlyingGivenIn(uint256 amountIn) public view returns (uint256) {
-        return amountIn * (BPS_SCALE - TO_UNDERLYING_FEE) / BPS_SCALE;
+        return amountIn * (BPS_SCALE - TO_UNDERLYING_FEE) / BPS_SCALE * conversionPrice / PRICE_SCALE;
     }
 
     function quoteToUnderlyingGivenOut(uint256 amountOut) public view returns (uint256) {
-        return amountOut * BPS_SCALE / (BPS_SCALE - TO_UNDERLYING_FEE);
+        return amountOut * BPS_SCALE / (BPS_SCALE - TO_UNDERLYING_FEE) * PRICE_SCALE / conversionPrice;
     }
 
     function quoteToSynthGivenIn(uint256 amountIn) public view returns (uint256) {
-        return amountIn * (BPS_SCALE - TO_SYNTH_FEE) / BPS_SCALE;
+        return amountIn * (BPS_SCALE - TO_SYNTH_FEE) / BPS_SCALE * PRICE_SCALE / conversionPrice;
     }
 
     function quoteToSynthGivenOut(uint256 amountOut) public view returns (uint256) {
-        return amountOut * BPS_SCALE / (BPS_SCALE - TO_SYNTH_FEE);
+        return amountOut * BPS_SCALE / (BPS_SCALE - TO_SYNTH_FEE) * conversionPrice / PRICE_SCALE;
     }
 }
