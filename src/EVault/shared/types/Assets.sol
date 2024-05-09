8d7200db2617aeba6bc04fa1d60c34d7b1d15b83
Doug Hoyte <doug@hcsw.org>
2024-05-09 16:42:29 -0400

    simplify borrow/repay events

--- src/EVault/shared/types/Assets.sol
+++ src/EVault/shared/types/Assets.sol
@@ -44,6 +44,12 @@ library AssetsLib {
         }
     }
 
+    function addUnchecked(Assets self, Assets b) internal pure returns (Assets) {
+        unchecked {
+            return Assets.wrap(uint112(self.toUint() + b.toUint()));
+        }
+    }
+
     function subUnchecked(Assets self, Assets b) internal pure returns (Assets) {
         unchecked {
             return Assets.wrap(uint112(self.toUint() - b.toUint()));
