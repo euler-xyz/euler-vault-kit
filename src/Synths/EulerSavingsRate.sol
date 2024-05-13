euler-xyz/euler-vault-kit

commit f732e7ee20cb843a0c80062bb6194e40d08c9da8

Avoid using += and -= operators
spearbit-audits/review-euler#113

src/Synths/EulerSavingsRate.sol
@@ -113,6 +113,7 @@ contract EulerSavingsRate is EVCUtil, ERC4626 {
     }
 
     function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal override {
+        totalAssetsDeposited = totalAssetsDeposited + assets;
         super._deposit(caller, receiver, assets, shares);
         totalAssetsDeposited += assets;
     }
@@ -121,6 +122,7 @@ contract EulerSavingsRate is EVCUtil, ERC4626 {
         internal
         override
     {
+        totalAssetsDeposited = totalAssetsDeposited - assets;
         super._withdraw(caller, receiver, owner, assets, shares);
         totalAssetsDeposited -= assets;
     }
@@ -151,7 +153,7 @@ contract EulerSavingsRate is EVCUtil, ERC4626 {
         // write esrSlotCache back to storage in a single SSTORE
         esrSlot = esrSlotCache;
         // Move interest accrued to totalAssetsDeposited
-        totalAssetsDeposited += accruedInterest;
+        totalAssetsDeposited = totalAssetsDeposited + accruedInterest;
 
         return esrSlotCache;
     }

commit 2e4dce112dc2ead9ccc86c3061b94539fa2c812a

Fix total assets mutations
fixes: f732e7ee20cb843a0c80062bb6194e40d08c9da8

src/Synths/EulerSavingsRate.sol
@@ -115,7 +115,6 @@ contract EulerSavingsRate is EVCUtil, ERC4626 {
     function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal override {
         totalAssetsDeposited = totalAssetsDeposited + assets;
         super._deposit(caller, receiver, assets, shares);
-        totalAssetsDeposited += assets;
     }
 
     function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares)
@@ -124,7 +123,6 @@ contract EulerSavingsRate is EVCUtil, ERC4626 {
     {
         totalAssetsDeposited = totalAssetsDeposited - assets;
         super._withdraw(caller, receiver, owner, assets, shares);
-        totalAssetsDeposited -= assets;
     }
 
     function gulp() public {