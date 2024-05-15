75271339ef397e8218e3a61b80145f1437c87337
dglowinski <dariushg32111@gmail.com>
2024-05-15 12:29:25 +0200

    fix rebase missing comments

--- src/interfaces/IFlashLoan.sol
+++ src/interfaces/IFlashLoan.sol
@@ -4,5 +4,7 @@ pragma solidity >=0.8.0;
 
 /// @notice Definition of callback method that flashLoan will invoke on your contract
 interface IFlashLoan {
+    /// @notice Function that will be called on the caller of `flashloan`
+    /// @param data Data that was passed to the `flashloan` call
     function onFlashLoan(bytes memory data) external;
 }
