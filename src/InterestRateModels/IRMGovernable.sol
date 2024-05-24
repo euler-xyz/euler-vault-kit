a728716e5f301c900a6bb28d6bf3ca5d313ac746
Mick de Graaf <mick@mickdegraaf.nl>
2024-05-24 14:10:35 +0200

    Governable Rate
    No tests yet

---
+++ src/InterestRateModels/IRMGovernable.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity ^0.8.0;
+
+import "./IIRM.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+
+/// @title IRMGovernable
+/// @custom:security-contact security@euler.xyz
+/// @author Euler Labs (https://www.eulerlabs.com/)
+/// @notice Implementaion on an interest rate model, which can be set by the owner of this contract
+contract IRMGovernable is IIRM {
+    uint256 public immutable maxRate;
+    uint256 public rate;
+
+    error RateExceedsMaxRate();
+    
+    constructor(uint256 maxRate_, uint256 initialRate) Ownable() {
+        maxRate = maxRate_;
+        initialRate = rate;
+    }
+
+    function setRate(uint256 newRate) external onlyOwner {
+        if (newRate > maxRate) {
+            revert RateExceedsMaxRate();
+        }
+        rate = newRate;
+    }
+
+    /// @inheritdoc IIRM
+    function computeInterestRate(address vault, uint256 cash, uint256 borrows)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return rate;
+    }
+
+    /// @inheritdoc IIRM
+    function computeInterestRateView(address vault, uint256 cash, uint256 borrows)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return rate;
+    }
+}
