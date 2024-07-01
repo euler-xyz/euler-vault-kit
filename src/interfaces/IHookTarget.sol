1c1a630c4bd6d1e73912e15f80faa440d8d74790
kasperpawlowski <kasperpawlowski@gmail.com>
2024-07-01 13:19:52 +0100

    fix: make isHookTarget view function

--- src/interfaces/IHookTarget.sol
+++ src/interfaces/IHookTarget.sol
@@ -10,5 +10,5 @@ interface IHookTarget {
     /// @notice If given contract is a hook target, it is expected to return the bytes4 magic value that is the selector
     /// of this function
     /// @return The bytes4 magic value (0x87439e04) that is the selector of this function
-    function isHookTarget() external returns (bytes4);
+    function isHookTarget() external view returns (bytes4);
 }
