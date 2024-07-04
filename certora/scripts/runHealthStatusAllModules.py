947180360d56dfc05a83b74f640e2ef519701b2a
Andrew Ferraiuolo <andrew@certora.com>
2024-07-04 11:54:26 +0100

    uncomment important bits of script

--- certora/scripts/runHealthStatusAllModules.py
+++ certora/scripts/runHealthStatusAllModules.py
@@ -7,46 +7,46 @@ parser.add_argument('-M', '--batchMsg', metavar='M', type=str, nargs='?',
                     help='a message for all the jobs')
 args = parser.parse_args()
 
-# hs_confs = [
-#     "BalanceForwarder",
-#     "Borrowing",
-#     "Governance",
-#     "Initialize",
-#     "Liquidation",
-#     "Token",
-#     "Vault"
-# ]
-# 
-# for conf in hs_confs:
-#     script = f"certora/conf/healthStatus/{conf}HealthStatus.conf"
-#     command = f"certoraRun {script} --msg \"{conf} : {args.batchMsg}\" --rule \"accountsStayHealthy_strategy\""
-#     print(f"runing {command}")
-#     subprocess.run(command, shell=True)
-# 
-# borrow_separate_methods = [
-#   "borrow(uint256,address)",
-#   "pullDebt(uint256,address)",
-#   "repayWithShares(uint256,address)",
-#   "repay(uint256,address)",
-# ]
-# 
-# for method in borrow_separate_methods:
-#     script = f"certora/conf/healthStatus/BorrowingHealthStatus.conf"
-#     command = f"certoraRun {script} --msg \"Borrow.{method} : {args.batchMsg}\" --rule \"accountsStayHealthy_strategy\" --method \"{method}\""
-#     print(f"runing {command}")
-#     subprocess.run(command, shell=True)
-# 
-# vault_separate_methods = [
-#   "redeem(uint256,address,address)",
-#   "withdraw(uint256,address,address)"
-# ]
-# 
-# for method in vault_separate_methods:
-#     script = f"certora/conf/healthStatus/VaultHealthStatus.conf"
-#     command = f"certoraRun {script} --msg \"Vault.{method} : {args.batchMsg}\" --rule \"accountsStayHealthy_strategy\" --method \"{method}\""
-#     print(f"runing {command}")
-#     subprocess.run(command, shell=True)
-# 
+hs_confs = [
+    "BalanceForwarder",
+    "Borrowing",
+    "Governance",
+    "Initialize",
+    "Liquidation",
+    "Token",
+    "Vault"
+]
+
+for conf in hs_confs:
+    script = f"certora/conf/healthStatus/{conf}HealthStatus.conf"
+    command = f"certoraRun {script} --msg \"{conf} : {args.batchMsg}\" --rule \"accountsStayHealthy_strategy\""
+    print(f"runing {command}")
+    subprocess.run(command, shell=True)
+
+borrow_separate_methods = [
+  "borrow(uint256,address)",
+  "pullDebt(uint256,address)",
+  "repayWithShares(uint256,address)",
+  "repay(uint256,address)",
+]
+
+for method in borrow_separate_methods:
+    script = f"certora/conf/healthStatus/BorrowingHealthStatus.conf"
+    command = f"certoraRun {script} --msg \"Borrow.{method} : {args.batchMsg}\" --rule \"accountsStayHealthy_strategy\" --method \"{method}\""
+    print(f"runing {command}")
+    subprocess.run(command, shell=True)
+
+vault_separate_methods = [
+  "redeem(uint256,address,address)",
+  "withdraw(uint256,address,address)"
+]
+
+for method in vault_separate_methods:
+    script = f"certora/conf/healthStatus/VaultHealthStatus.conf"
+    command = f"certoraRun {script} --msg \"Vault.{method} : {args.batchMsg}\" --rule \"accountsStayHealthy_strategy\" --method \"{method}\""
+    print(f"runing {command}")
+    subprocess.run(command, shell=True)
+
 liquidate_cases = [
     "liquidateAccountsStayHealthy_liquidator_no_debt_socialization",
     "liquidateAccountsStayHealthy_liquidator_with_debt_socialization",
