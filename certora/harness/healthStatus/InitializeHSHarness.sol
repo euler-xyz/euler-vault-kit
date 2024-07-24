d8e07b4e64a7dcaeb12237462f816a759f2072b4
Andrew Ferraiuolo <andrew@certora.com>
2024-07-24 11:49:57 +0100

    delete some commented code

--- certora/harness/healthStatus/InitializeHSHarness.sol
+++ certora/harness/healthStatus/InitializeHSHarness.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 import "../../../src/interfaces/IPriceOracle.sol";
-// import {ERC20} from "../../../lib/ethereum-vault-connector/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "../../../certora/harness/AbstractBaseHarness.sol";
 import "../../../src/EVault/modules/RiskManager.sol";
 import "../../../src/EVault/modules/Initialize.sol";
