4f2fc1d7dd1c0df6faf2f5c76a9c6b2376831416
Andrew Ferraiuolo <andrew@certora.com>
2024-07-24 11:49:57 +0100

    delete some commented code

--- certora/harness/ERC4626Harness.sol
+++ certora/harness/ERC4626Harness.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 pragma solidity ^0.8.0;
-// import {IERC20} from "../../lib/ethereum-vault-connector/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "../../certora/harness/AbstractBaseHarness.sol";
 import "../../src/EVault/modules/Vault.sol";
 import "../../src/EVault/modules/Token.sol";
-// import "../../src/EVault/shared/types/Types.sol";
 
 contract ERC4626Harness is VaultModule, TokenModule, AbstractBaseHarness {
     constructor(Integrations memory integrations) Base(integrations) {}
