927da760f25a2fddbd654388dc87320829305518
Andrew Ferraiuolo <andrew@certora.com>
2024-08-02 09:48:58 +0100

    Rule version of specs for withdraw

--- certora/scripts/runERC4626RulesSplitConfs.py
+++ certora/scripts/runERC4626RulesSplitConfs.py
@@ -7,20 +7,22 @@ parser.add_argument('-M', '--batchMsg', metavar='M', type=str, nargs='?',
                     help='a message for all the jobs')
 
 erc4626_confs = {
-    # "",
-    # "-assetsMoreThanSupply",
-    # "-convertToAssetsWeakAdditivity",
-    # "-convertToSharesWeakAdditivity",
-    # "-depositMonotonicity",
-    # "-dustFavorsTheHouse",
-    # "-dustFavorsTheHouseAssets",
-    # "-noAssetsIfNoSupply",
-    # "-noSupplyIfNoAssets",
-    # "-onlyContributionMethodsReduce",
-    # "-totalsMonotonicity",
+    "",
+    "-assetsMoreThanSupply",
+    "-convertToAssetsWeakAdditivity",
+    "-convertToSharesWeakAdditivity",
+    "-depositMonotonicity",
+    "-dustFavorsTheHouse",
+    "-dustFavorsTheHouseAssets",
+    "-noAssetsIfNoSupply",
+    "-noSupplyIfNoAssets",
+    "-onlyContributionMethodsReduce",
+    "-totalsMonotonicity",
     "-vaultSolvency-most",
     "-vaultSolvency-redeem",
     "-vaultSolvency-withdraw"
+    # In case the invariant times out for withdraw
+    "-vaultSolvency-withdraw-as-rule"
 }
 
 for name in erc4626_confs:
@@ -29,3 +31,4 @@ for name in erc4626_confs:
     command = f"certoraRun {script} --msg \"{name} : {args.batchMsg}\""
     print(f"runing {command}")
     subprocess.run(command, shell=True)
+
