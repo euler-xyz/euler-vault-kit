418420a1ee75dc1e1a25d53923ba54014c11d550
alphak3y <alphak3y@protonmail.com>
2025-12-17 05:52:30 -0700

    usdxl minting module

--- src/EVault/shared/BorrowUtils.sol
+++ src/EVault/shared/BorrowUtils.sol
@@ -108,6 +108,8 @@ abstract contract BorrowUtils is Base {
         logBorrow(to, borrowAssets, toPrevAssets, toAssets);
     }
 
+    //TODO: need interest rate controller with IIRM.computeInterestRate support
+
     function computeInterestRate(VaultCache memory vaultCache) internal virtual returns (uint256) {
         // single sload
         address irm = vaultStorage.interestRateModel;
@@ -130,6 +132,8 @@ abstract contract BorrowUtils is Base {
 
         return newInterestRate;
     }
+
+    //TODO: need interest rate controller with IIRM.computeInterestRateView support
 
     function computeInterestRateView(VaultCache memory vaultCache) internal view virtual returns (uint256) {
         // single sload
